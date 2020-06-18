021d5553b69628add0081eef169b638474bb1529
Tim Untersberger <timuntersberger2@gmail.com>
2020-06-18 20:17:13 +0200

    Start implementation of the git_status function

--- lua/neogit/init.lua
+++ lua/neogit/init.lua
@@ -1,1 +1,25 @@
+local branch_re = "On branch \\(\\w\\+\\)"
+local remote_re = "Your branch is up to date with '\\(.*\\)'."
+local modified_re = ".*modified:\\W*\\(.*\\)"
 
+local function git_status()
+  local result = {}
+  local output = vim.fn.systemlist("git status")
+
+  result.branch = vim.fn.matchlist(output[1], branch_re)[2]
+  result.remote = vim.fn.matchlist(output[2], remote_re)[2]
+
+  local i = 7
+
+  result.modified_files = {}
+
+  while output[i] ~= "" do
+    local file = vim.fn.matchlist(output[i], modified_re)[2]
+    table.insert(result.modified_files, file)
+    i = i + 1
+  end
+
+  return result
+end
+
+print(vim.inspect(git_status()))
