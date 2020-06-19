9f4152111d08713247b2b091b1690d735205ccaf
Tim Untersberger <timuntersberger2@gmail.com>
2020-06-19 11:47:23 +0200

    Update init.lua

--- lua/neogit/init.lua
+++ lua/neogit/init.lua
@@ -1,22 +1,46 @@
 local branch_re = "On branch \\(\\w\\+\\)"
-local remote_re = "Your branch is \\(up to date with\\|ahead of\\) '\\(.*\\)' \\?\\(by \\(\\d*\\) commit\\)\\?"
+local remote_re = "Your branch is \\(up to date with\\|ahead of\\|behind\\) '\\(.*\\)' \\?\\(by \\(\\d*\\) commit\\)\\?"
 local change_re = "\\W*\\(.*\\):\\W*\\(.*\\)"
 
 local function git_status()
   local output = vim.fn.systemlist("git status")
-
-  local result = {}
   local lineidx = 1
 
-  result.committed_changes = {}
-  result.uncommitted_changes = {}
+  local function parse_changes(list)
+    while output[lineidx] ~= "" do
+      local matches = vim.fn.matchlist(output[lineidx], change_re)
+      local type = matches[2]
+      local file = matches[3]
+      table.insert(list, { type = type, file = file })
+      lineidx = lineidx + 1
+    end
+  end
+
+  local function skip_explanation()
+    while string.find(output[lineidx], "\t") == nil do
+      lineidx = lineidx + 1
+    end
+  end
+
+  local result = {}
+
+  result.staged_changes = {}
+  result.unstaged_changes = {}
   result.untracked_files = {}
+  result.ahead_by = 0
+  result.behind_by = 0
 
   result.branch = vim.fn.matchlist(output[lineidx], branch_re)[2]
   lineidx = lineidx + 1
 
   local matches = vim.fn.matchlist(output[lineidx], remote_re)
-  result.ahead_by = tonumber(matches[5]) or 0
+
+  if matches[2] == "ahead of" then
+    result.ahead_by = tonumber(matches[5])
+  elseif matches[2] == "behind" then
+    result.behind_by = tonumber(matches[5])
+  end
+
   result.remote = matches[3]
   lineidx = lineidx + 1
 
@@ -27,35 +51,23 @@ local function git_status()
   lineidx = lineidx + 1
 
   if output[lineidx] == "Changes to be committed:" then
-    lineidx = lineidx + 2 -- skip explanation
+    skip_explanation()
 
-    while output[lineidx] ~= "" do
-      local matches = vim.fn.matchlist(output[lineidx], change_re)
-      local type = matches[2]
-      local file = matches[3]
-      table.insert(result.committed_changes, { type = type, file = file })
-      lineidx = lineidx + 1
-    end
+    parse_changes(result.staged_changes)
 
     lineidx = lineidx + 1
   end
 
   if output[lineidx] == "Changes not staged for commit:" then
-    lineidx = lineidx + 3 -- skip explanation
+    skip_explanation()
 
-    while output[lineidx] ~= "" do
-      local matches = vim.fn.matchlist(output[lineidx], change_re)
-      local type = matches[2]
-      local file = matches[3]
-      table.insert(result.uncommitted_changes, { type = type, file = file })
-      lineidx = lineidx + 1
-    end
+    parse_changes(result.unstaged_changes)
 
     lineidx = lineidx + 1
   end
 
   if output[lineidx] == "Untracked files:" then
-    lineidx = lineidx + 2 -- skip explanation
+    skip_explanation()
 
     while output[lineidx] ~= "" do
       local file = string.sub(output[lineidx], 2)
@@ -67,4 +79,30 @@ local function git_status()
   return result
 end
 
-print(vim.inspect(git_status()))
+local function git_tree()
+  local output = vim.fn.systemlist("git log --graph --pretty=oneline --abbrev-commit")
+  return output
+end
+
+local function git_fetch()
+  local output = vim.fn.systemlist("git fetch")
+  return output
+end
+
+local function git_unpulled(branch)
+  local output = vim.fn.systemlist("git log --oneline .." .. branch)
+  return output
+end
+
+local function git_unmerged(branch)
+  local output = vim.fn.systemlist("git log --oneline " .. branch .. "..")
+  return output
+end
+
+return {
+  status = git_status,
+  fetch = git_fetch,
+  unpulled = git_unpulled,
+  unmerged = git_unmerged,
+  tree = git_tree
+}
--- plugin/neogit.vim
+++ plugin/neogit.vim
@@ -1,1 +1,56 @@
+lua neogit = require("neogit")
 
+function! s:neogit()
+  let status = luaeval("neogit.status()")
+  let s:lineidx = 0
+
+  echo status
+
+  enew
+
+  function! Write(str)
+    call append(s:lineidx, a:str)
+    let s:lineidx = s:lineidx + 1
+  endfunction
+
+  call Write("Head: " . status.branch)
+  call Write("Push: " . status.remote)
+
+  if len(status.unstaged_changes) != 0
+    call Write("")
+    call Write("Unstaged changes (" . len(status.unstaged_changes) . ")")
+    for change in status.unstaged_changes
+      call Write(change.type . " " . change.file)
+    endfor
+  endif
+
+  if len(status.staged_changes) != 0
+    call Write("")
+    call Write("Staged changes (" . len(status.staged_changes) . ")")
+    for change in status.staged_changes
+      call Write(change.type . " " . change.file)
+    endfor
+  endif
+
+  if status.behind_by != 0
+    call Write("")
+    call Write("Unpulled from " . status.remote . " (" . status.behind_by . ")")
+
+    let commits = luaeval("neogit.unpulled('" . status.remote . "')")
+
+    for commit in commits
+      call Write(commit)
+    endfor
+  endif
+
+  setlocal nomodifiable
+  setlocal nohidden
+  setlocal noswapfile
+  setlocal nobuflisted
+
+  nnoremap <buffer> <silent> q :bd!<CR>
+endfunction
+
+command! -nargs=0 Neogit call <SID>neogit()
+
+Neogit
