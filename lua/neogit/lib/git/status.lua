8effd71ed69de57cce22554152b4cf1cc4062750
Tim Untersberger <timuntersberger2@gmail.com>
2020-07-18 11:45:49 +0200

    refactor: change how we get the status information
    Before this commit we used to parse the output of `git status`.
    The parsing of this output was really annoying and bad practice so now
    we parse the output of `git status --porcelain=1 --branch` which is way
    easier, meaning also faster.

--- lua/neogit/lib/git/status.lua
+++ lua/neogit/lib/git/status.lua
@@ -1,8 +1,46 @@
 local cli = require("neogit.lib.git.cli")
 
+local function marker_to_type(m)
+  if m == "M" then
+    return "modified"
+  elseif m == "A" then
+    return "new file"
+  elseif m == "D" then
+    return "deleted"
+  else
+    return "unknown"
+  end
+end
+
 local status = {
   get = function()
     local output = cli.run("status --porcelain=1 --branch")
+    local result = {
+      unstaged_changes = {},
+      staged_changes = {},
+      untracked_files = {},
+      ahead_by = 0,
+      behind_by = 0,
+      branch = "",
+      remote = ""
+    }
+
+    local function insert_change(list, marker, name)
+      table.insert(list, {
+        type = marker_to_type(marker),
+        name = name,
+        diff_height = 0,
+        diff_open = false
+      })
+    end
+
+    local function set_diversion(type, value)
+      if type == "behind" then
+        result.behind_by = tonumber(value)
+      elseif type == "ahead" then
+        result.ahead_by = tonumber(value)
+      end
+    end
 
     for _, line in pairs(output) do
       local matches = vim.fn.matchlist(line, "\\(.\\{2}\\) \\(.*\\)")
@@ -10,16 +48,19 @@ local status = {
       local details = matches[3]
 
       if marker == "##" then
-        matches = vim.fn.matchlist(details, "^\\([a-zA-Z0-9]*\\)...\\(\\S*\\)\\%( \\[\\(ahead\\|behind\\) \\([0-9]*\\)\\]\\)*$")
-        print(vim.inspect(matches))
+        matches = vim.fn.matchlist(details, "^\\([a-zA-Z0-9]*\\)...\\(\\S*\\) \\?\\[\\%(\\(ahead\\) \\([0-9]*\\)\\%(, \\)\\?\\)\\?\\%(\\(behind\\) \\([0-9]*\\)\\)\\?\\]")
+        result.branch = matches[2]
+        result.remote = matches[3]
+        set_diversion(matches[4], matches[5])
+        set_diversion(matches[6], matches[7])
       elseif marker == "??" then
-        print(details .. " is untracked")
+        insert_change(result.untracked_files, "A", details)
       else
         local chars = vim.split(marker, "")
         if chars[1] == " " then
-          print(details .. " is not staged")
+          insert_change(result.unstaged_changes, chars[2], details)
         else
-          print(details .. " is staged")
+          insert_change(result.staged_changes, chars[1], details)
         end
       end
     end
@@ -42,7 +83,5 @@ local status = {
     cli.run("reset")
   end,
 }
-
-status.get()
 
 return status
