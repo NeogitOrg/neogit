a7ec82ba4367f17212d682f8b0ce742f38624df3
Tim Untersberger <timuntersberger2@gmail.com>
2020-07-20 13:25:09 +0200

    feat(cli): add a cli history
    To get the history of commands run the following lua script
    
    ```lua
    print(vim.inspect(require("neogit.lib.git.cli").history))
    ```

--- lua/neogit/lib/git/cli.lua
+++ lua/neogit/lib/git/cli.lua
@@ -1,5 +1,15 @@
-return {
+local history = {}
+local cli = {
   run = function(cmd)
-    return vim.fn.systemlist("git " .. cmd)
+    local output = vim.fn.systemlist("git " .. cmd)
+    table.insert(history, {
+      cmd = "git " .. cmd,
+      output = output,
+      code = vim.v.shell_error
+    })
+    return output
   end,
+  history = history
 }
+
+return cli
