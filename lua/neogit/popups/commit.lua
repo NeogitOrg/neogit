f4a8633b03e087f78cbadc4d09916e09542131e4
TimUntersberger <timuntersberger2@gmail.com>
2021-01-24 12:57:22 +0100

    feat: implement git commit --amend

--- lua/neogit/popups/commit.lua
+++ lua/neogit/popups/commit.lua
@@ -3,6 +3,52 @@ local cli = require("neogit.lib.git.cli")
 local util = require("neogit.lib.util")
 local Buffer = require("neogit.lib.buffer")
 
+local function create_commit_window(msg, commit_cb)
+  local output = {}
+
+  inspect(popup.to_cli())
+
+  if msg then
+    for _, line in ipairs(msg) do
+      table.insert(output, line)
+    end
+  else
+    table.insert(output, "")
+  end
+
+  table.insert(output, "# Please enter the commit message for your changes. Lines starting")
+  table.insert(output, "# with '#' will be ignored, and an empty message aborts the commit.")
+
+  for _, line in pairs(cli.run("status")) do
+    if not vim.startswith(line, "  (") then
+      table.insert(output, "# " .. line)
+    end
+  end
+
+  Buffer.create {
+    name = ".git/COMMIT_EDITMSG",
+    filetype = "gitcommit",
+    modifiable = true,
+    readonly = false,
+    initialize = function(buffer)
+      buffer:set_lines(0, -1, false, output)
+
+      local mappings = buffer.mmanager.mappings
+
+      mappings["control-c control-c"] = function()
+        vim.cmd([[
+          silent set buftype=
+          silent g/^#/d
+          silent w!
+          silent bw!
+        ]])
+
+        commit_cb()
+      end
+    end
+  }
+end
+
 local function create()
   popup.create(
     "NeogitCommitPopup",
@@ -70,43 +116,13 @@ local function create()
           key = "c",
           description = "Commit",
           callback = function(popup)
-            local output = {
-              "",
-              "# Please enter the commit message for your changes. Lines starting",
-              "# with '#' will be ignored, and an empty message aborts the commit."
-            }
-
-            for _, line in pairs(cli.run("status")) do
-              if not vim.startswith(line, "  (") then
-                table.insert(output, "# " .. line)
-              end
-            end
-
-            Buffer.create {
-              name = ".git/COMMIT_EDITMSG",
-              filetype = "gitcommit",
-              modifiable = true,
-              readonly = false,
-              initialize = function(buffer)
-                buffer:set_lines(0, -1, false, output)
-
-                local mappings = buffer.mmanager.mappings
-
-                mappings["control-c control-c"] = function()
-                  vim.cmd([[
-                    silent set buftype=
-                    silent g/^#/d
-                    silent w!
-                    silent bw!
-                  ]])
-                  cli.run("commit -F .git/COMMIT_EDITMSG " .. popup.to_cli(), function(_, code)
-                    if code == 0 then
-                      __NeogitStatusRefresh(true)
-                    end
-                  end)
+            create_commit_window(nil, function()
+              cli.run("commit -F .git/COMMIT_EDITMSG " .. popup.to_cli(), function(_, code)
+                if code == 0 then
+                  __NeogitStatusRefresh(true)
                 end
-              end
-            }
+              end)
+            end)
           end
         },
       },
@@ -124,7 +140,16 @@ local function create()
         {
           key = "a",
           description = "Amend",
-          callback = function() end
+          callback = function(popup) 
+            local msg = cli.run("log -1 --pretty=%B")
+            create_commit_window(msg, function()
+              cli.run("commit -F .git/COMMIT_EDITMSG --amend", function(_, code)
+                if code == 0 then
+                  __NeogitStatusRefresh(true)
+                end
+              end)
+            end)
+          end
         },
       },
       {
