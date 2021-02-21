ff7c7add3c88771f4e2f8e9d1534e804d4f7f398
Florian Proksch <florian.proksch@protonmail.com>
2021-02-21 10:21:03 +0100

    refactor: Add async cli calls to history

--- lua/neogit/lib/git/cli.lua
+++ lua/neogit/lib/git/cli.lua
@@ -4,26 +4,12 @@ local util = require("neogit.lib.util")
 local a = require('neogit.async')
 local process = require('neogit.process')
 
-local function get_root_path()
-  local job = Job:new("git rev-parse --show-toplevel")
-
-  job:start()
-  job:wait()
-
-  local path = job.stdout[1]
-
-  return path
-end
 local git_root = a.sync(function()
   return vim.trim(a.wait(process.spawn({cmd = 'git', args = {'rev-parse', '--show-toplevel'}})))
 end)
 
 
 local history = {}
-
-local function prepend_git(x)
-  return "git " .. x
-end
 
 local function handle_new_cmd(job, popup)
   if popup == nil then
@@ -39,7 +25,9 @@ local function handle_new_cmd(job, popup)
   })
 
   if popup and job.code ~= 0 then
-    notif.create({ "Git Error (" .. job.code .. ")!", "", "Press $ to see the git command history." }, { type = "error" })
+    vim.schedule(function ()
+      notif.create({ "Git Error (" .. job.code .. ")!", "", "Press $ to see the git command history." }, { type = "error" })
+    end)
   end
 end
 
@@ -47,12 +35,20 @@ local exec = a.sync(function(cmd, args, cwd, stdin)
   args = args or {}
   table.insert(args, 1, cmd)
 
+  local time = os.clock()
   local result, code, errors = a.wait(process.spawn({
     cmd = 'git',
     args = args,
     input = stdin,
     cwd = cwd or a.wait(git_root())
   }))
+  handle_new_cmd({
+    cmd = cmd .. ' ' .. table.concat(args, ' '),
+    stdout = vim.split(result, '\n'),
+    stderr = vim.split(errors, '\n'),
+    code = code,
+    time = os.clock() - time
+  }, true)
   --print('git', table.concat(args, ' '), '->', code, errors)
 
   return result, code, errors
