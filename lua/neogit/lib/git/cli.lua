NeogitOrg/neogit

commit ff7c7add3c88771f4e2f8e9d1534e804d4f7f398

refactor: Add async cli calls to history

lua/neogit/lib/git/cli.lua
@@ -4,26 +4,12 @@ local util = require("neogit.lib.util")
 local a = require('neogit.async')
 local process = require('neogit.process')
 
-local function get_root_path()
-  local job = Job:new("git rev-parse --show-toplevel")
-
-  job:start()
-  job:wait()
-
-  local path = job.stdout[1]
-
-  return path
-end
 local git_root = a.sync(function()
   return vim.trim(a.wait(process.spawn({cmd = 'git', args = {'rev-parse', '--show-toplevel'}})))
 end)
 
 
 local history = {}
-
-local function prepend_git(x)
-  return "git " .. x
-end
 
 local function handle_new_cmd(job, popup)
   if popup == nil then
@@ -39,7 +25,9 @@ local function handle_new_cmd(job, popup)
   })
 
   if popup and job.code ~= 0 then
-    notif.create({ "Git Error (" .. job.code .. ")!", "", "Press $ to see the git command history." }, { type = "error" })
+    vim.schedule(function ()
+      notif.create({ "Git Error (" .. job.code .. ")!", "", "Press $ to see the git command history." }, { type = "error" })
+    end)
   end
 end
 
@@ -47,12 +35,20 @@ local exec = a.sync(function(cmd, args, cwd, stdin)
   args = args or {}
   table.insert(args, 1, cmd)
 
+  local time = os.clock()
   local result, code, errors = a.wait(process.spawn({
     cmd = 'git',
     args = args,
     input = stdin,
     cwd = cwd or a.wait(git_root())
   }))
+  handle_new_cmd({
+    cmd = cmd .. ' ' .. table.concat(args, ' '),
+    stdout = vim.split(result, '\n'),
+    stderr = vim.split(errors, '\n'),
+    code = code,
+    time = os.clock() - time
+  }, true)
   --print('git', table.concat(args, ' '), '->', code, errors)
 
   return result, code, errors

commit 440659bf96e00ae089645c3bb330094098f0f7a4

fix(#48): prevent popups from showing for status
Fix a regression from ff7c7add3c88771f4e2f8e9d1534e804d4f7f398
Calls resulting from synchronizing git status no longer cause popups to
show if any of the commands fail.

lua/neogit/lib/git/cli.lua
@@ -111,8 +111,9 @@ local function handle_new_cmd(job, popup)
   end
 end
 
-local exec = a.sync(function(cmd, args, cwd, stdin)
+local exec = a.sync(function(cmd, args, cwd, stdin, show_popup)
   args = args or {}
+  if show_popup == nil then show_popup = true end
   table.insert(args, 1, cmd)
 
   local time = os.clock()
@@ -128,7 +129,7 @@ local exec = a.sync(function(cmd, args, cwd, stdin)
     stderr = split(errors, '\n'),
     code = code,
     time = os.clock() - time
-  }, true)
+  }, show_popup)
   --print('git', table.concat(args, ' '), '->', code, errors)
 
   return result, code, errors
@@ -168,6 +169,13 @@ local mt_builder = {
     if action == 'cwd' then
       return function (cwd)
         tbl[k_state].cwd = cwd
+        return tbl
+      end
+    end
+
+    if action == 'show_popup' then
+      return function (show_popup)
+        tbl[k_state].show_popup = show_popup
         return tbl
       end
     end
@@ -215,6 +223,7 @@ local function new_builder(subcommand)
     arguments = {},
     files = {},
     input = nil,
+    show_popup = true,
     cwd = nil
   }
 
@@ -229,7 +238,7 @@ local function new_builder(subcommand)
       table.insert(args, '--')
       for _,f in ipairs(state.files) do table.insert(args, f) end
 
-      return a.wait(exec(subcommand, args, state.cwd, state.input))
+      return a.wait(exec(subcommand, args, state.cwd, state.input, state.show_popup))
     end)
   }, mt_builder)
 end
@@ -237,6 +246,7 @@ end
 local function new_parallel_builder(calls)
   local state = {
     calls = calls,
+    show_popup = true,
     cwd = nil
   }
 
@@ -249,7 +259,7 @@ local function new_parallel_builder(calls)
     if not state.cwd or state.cwd == "" then return end
 
     for _,c in ipairs(state.calls) do
-      c.cwd(state.cwd)
+      c.cwd(state.cwd).show_popup(state.show_popup)
     end
 
     local processes = {}
@@ -267,6 +277,13 @@ local function new_parallel_builder(calls)
       if action == 'cwd' then
         return function (cwd)
           state.cwd = cwd
+          return tbl
+        end
+      end
+
+      if action == 'show_popup' then
+        return function (show_popup)
+          state.show_popup = show_popup
           return tbl
         end
       end

lua/neogit/lib/git/status.lua
@@ -28,7 +28,7 @@ local status = {
       git.cli.log.oneline.for_range('..@{upstream}'),
       git.cli.log.max_count(1).pretty('%B'),
       git.cli.log.max_count(1).pretty('%B').for_range('@{upstream}')
-    ).call())
+    ).show_popup(false).call())
 
     if status == nil then return nil end
 