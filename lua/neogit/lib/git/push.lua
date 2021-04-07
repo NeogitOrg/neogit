fa941274218fb16464072805a17ba80e7c6f2648
Florian Proksch <florian.proksch@protonmail.com>
2021-04-07 20:19:27 +0200

    fix(#104): untangle unpulled/unmerged
    Closes #104

--- lua/neogit/lib/git/push.lua
+++ lua/neogit/lib/git/push.lua
@@ -8,9 +8,9 @@ local update_unmerged = a.sync(function (state)
   if not state.upstream.branch then return end
 
   local result = a.wait(
-    cli.log.oneline.for_range('..@{upstream}').show_popup(false).call())
+    cli.log.oneline.for_range('@{upstream}..').show_popup(false).call())
 
-  state.unpulled.files = util.map(util.split(result, '\n'), function (x) return { name = x } end)
+  state.unmerged.files = util.map(util.split(result, '\n'), function (x) return { name = x } end)
 end)
 
 function M.register(meta)
