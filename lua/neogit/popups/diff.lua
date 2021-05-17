13d5daf80aa797ca8274bb1a79e9ad59f6163f87
Saad Parwaiz <saad.parwaiz1@gmail.com>
2021-05-17 12:34:27 +0100

    fix incorrect call to diffview module
    requiring diffview module at the start causes errors if integration
    is not enabled.

--- lua/neogit/popups/diff.lua
+++ lua/neogit/popups/diff.lua
@@ -1,6 +1,5 @@
 local M = {}
 
-local dv = require 'neogit.integrations.diffview'
 local config = require 'neogit.config'
 local popup = require 'neogit.lib.popup'
 
@@ -12,7 +11,7 @@ function M.create()
   return popup.new()
     .name("NeogitDiffPopup")
     .action("D", "diff against head", function()
-      dv.open()
+      require 'neogit.integrations.diffview'.open()
     end)
     .build()
 end
