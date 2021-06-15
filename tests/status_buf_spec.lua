3336190e0ed2750a960034743b7e28ef259293d6
TimUntersberger <timuntersberger2@gmail.com>
2021-06-15 19:52:39 +0200

    test

--- tests/status_buf_spec.lua
+++ tests/status_buf_spec.lua
@@ -1,5 +1,6 @@
 local eq = assert.are.same
 local status = require'neogit.status'
+
 local harness = require'tests.git_harness'
 local _ = require 'tests.mocks.input'
 local in_prepared_repo = harness.in_prepared_repo
