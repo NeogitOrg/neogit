3fab92b376655277eec15d36b75f850176b6f4d4
Tei Roberts <ten3roberts@gmail.com>
2022-09-16 15:45:31 +0200

    chore: remove dead code

--- lua/neogit/popups/commit.lua
+++ lua/neogit/popups/commit.lua
@@ -18,43 +18,6 @@ local get_commit_message = a.wrap(function(content, cb)
   CommitEditorBuffer.new(content, get_commit_file(), cb):open()
 end, 2)
 
--- If skip_gen is true we don't generate the massive git comment.
--- This flag should be true when the file already exists
-local function prompt_commit_message(args, msg, skip_gen)
-  local msg_template_path = cli.config.get("commit.template").show_popup(false).call()[1]
-  local output = {}
-
-  if msg and #msg > 0 then
-    for _, line in ipairs(msg) do
-      table.insert(output, line)
-    end
-  elseif not skip_gen and not msg_template_path then
-    table.insert(output, "")
-  end
-
-  if not skip_gen then
-    if msg_template_path then
-      a.util.scheduler()
-      local expanded_path = vim.fn.glob(msg_template_path)
-      if expanded_path == "" then
-        return
-      end
-      local msg_template = uv_utils.read_file_sync(expanded_path)
-      for _, line in pairs(msg_template) do
-        table.insert(output, line)
-      end
-      table.insert(output, "")
-    end
-    local lines = cli.commit.dry_run.args(unpack(args)).call()
-    for _, line in ipairs(lines) do
-      table.insert(output, "# " .. line)
-    end
-  end
-
-  a.util.scheduler()
-  return get_commit_message(output)
-end
-
 local function do_commit(popup, cmd)
   a.util.scheduler()
 
@@ -76,27 +39,6 @@ local function do_commit(popup, cmd)
   end
   a.util.scheduler()
   status.refresh(true)
-  -- local commit_file = get_commit_file()
-  -- if data then
-  --   local ok = prompt_commit_message(popup:get_arguments(), data, skip_gen)
-  --   if not ok then
-  --     return
-  --   end
-  -- end
-  -- a.util.scheduler()
-  -- local notification = notif.create("Committing...", vim.log.levels.INFO, 9999)
-  -- local result = cli.interactive_git_cmd(cmd)
-  -- a.util.scheduler()
-  -- if notification then
-  --   notification:delete()
-  -- end
-  --
-  -- if result.code == 0 then
-  --   notif.create("Successfully committed!")
-  --   a.uv.fs_unlink(commit_file)
-  --   status.refresh(true)
-  --   vim.cmd([[do <nomodeline> User NeogitCommitComplete]])
-  -- end
 end
 
 function M.create()
@@ -115,20 +57,6 @@ function M.create()
     :option("C", "reuse-message", "", "Reuse commit message")
     :action("c", "Commit", function(popup)
       do_commit(popup, cli.commit)
-      -- vim.notify("Getting commit file")
-      -- local commit_file = get_commit_file()
-      -- local _, data = uv_utils.read_file(commit_file)
-      -- vim.notify("Read commit data: " .. vim.inspect(data))
-      -- local skip_gen = data ~= nil
-      -- data = data or ""
-      -- -- we need \r? to support windows
-      -- data = split(data, "\r?\n")
-      -- do_commit(
-      --   popup,
-      --   data,
-      --   tostring(cli.commit.commit_message_file(commit_file).args(unpack(popup:get_arguments()))),
-      --   skip_gen
-      -- )
     end)
     :action("e", "Extend", function(popup)
       do_commit(popup, cli.commit.no_edit.amend)
