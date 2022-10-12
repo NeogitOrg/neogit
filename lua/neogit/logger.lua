1111e8d7fa2d7e35f0db0990e4375397b9a6f1a6
Tei Roberts <ten3roberts@gmail.com>
2022-10-12 14:07:53 +0200

    chore: remove logs to console

--- lua/neogit/logger.lua
+++ lua/neogit/logger.lua
@@ -3,6 +3,6 @@ local log = require("plenary.log")
 return log.new {
   plugin = "neogit",
   highlights = false,
-  use_console = true,
-  level = "info",
+  use_console = false,
+  level = "debug",
 }
