ef43bbd77de7f2e6fa12fb08d31ef12fa63445dd
Tei Roberts <ten3roberts@gmail.com>
2022-11-02 10:46:21 +0100

    feat: make logging configurable through env

--- lua/neogit/logger.lua
+++ lua/neogit/logger.lua
@@ -3,7 +3,7 @@ local log = require("plenary.log")
 return log.new {
   plugin = "neogit",
   highlights = false,
-  use_console = false,
-  use_file = false,
-  level = "debug",
+  use_console = vim.env.NEOGIT_LOG_ENABLE or false,
+  use_file = vim.env.NEOGIT_LOG_ENABLE or false,
+  level = vim.env.NEOGIT_LOG_LEVEL or "info",
 }
