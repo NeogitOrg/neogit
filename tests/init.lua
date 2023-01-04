55ad6d0098b7cf99ee0c0b47d2c51abfb9765ec0
Tei Roberts <ten3roberts@gmail.com>
2023-01-04 17:20:28 +0100

    chore: fix ci

--- tests/init.lua
+++ tests/init.lua
@@ -1,14 +1,16 @@
 local cwd = vim.fn.getcwd()
+local plenary_path = cwd .. "/tmp/plenary"
 
+print("Downloading plenary into: ", plenary_path)
 vim.fn.system {
   "git",
   "clone",
   "--depth=1",
   "https://github.com/nvim-lua/plenary.nvim",
-  vim.fn.getcwd() .. "/tmp/plenary",
+  plenary_path,
 }
 
-vim.opt.rtp:prepend(vim.fn.getcwd() .. "/tmp/plenary")
-vim.opt.rtp:prepend(vim.fn.getcwd())
+vim.opt.rtp:prepend(plenary_path)
+vim.opt.rtp:prepend(cwd)
 
 vim.cmd("runtime plugin/neogit.lua")
