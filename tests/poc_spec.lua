de227f740bb29f90d62f9a6112f926de5f052358
Tei Roberts <ten3roberts@gmail.com>
2023-01-23 15:33:03 +0100

    fix: tests

--- tests/poc_spec.lua
+++ tests/poc_spec.lua
@@ -16,14 +16,13 @@ describe("proof of concept", function()
 
   it("has access to buffers", function()
     vim.cmd("Neogit")
-    -- 1 is most likely the initial buffer nvim openes when starting?
-    -- 2 is the neogit buffer just opened
-    -- 3 The hidden console buffer
+    -- 1 is the neogit buffer just opened
+    -- 2 The hidden console buffer
     for _, b in ipairs(vim.api.nvim_list_bufs()) do
       local name = vim.api.nvim_buf_get_name(b)
       local ft = vim.api.nvim_buf_get_option(b, "filetype")
       local buftype = vim.api.nvim_buf_get_option(b, "buftype")
     end
-    eq({ 1, 2, 3 }, vim.api.nvim_list_bufs())
+    eq({ 1, 2 }, vim.api.nvim_list_bufs())
   end)
 end)
