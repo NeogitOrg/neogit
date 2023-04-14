f8ad8b0baa204adbd24b724b415dacd6657e3e2d
Cameron <Alleyria@gmail.com>
2023-04-14 16:44:55 +0200

    Clean up merge lib/popup

--- lua/neogit/popups/merge.lua
+++ lua/neogit/popups/merge.lua
@@ -1,44 +1,61 @@
 local git = require("neogit.lib.git")
 local popup = require("neogit.lib.popup")
-local merge = require("neogit.lib.git.merge")
 
 local M = {}
 local a = require("plenary.async")
 
+local FuzzyFinderBuffer = require("neogit.buffers.fuzzy_finder")
+
+local function in_merge(status)
+  return status and status.repo.rebase.head
+end
+
 function M.create()
   local status = require("neogit.status")
-  local p = popup
-    .builder()
-    :name("NeogitMergePopup")
-    :action_if(status and status.repo.merge.head, "m", "Continue merge", function()
-      merge.continue()
-      a.util.scheduler()
-      status.refresh(true, "merge_continue")
-    end)
-    :action_if(status and status.repo.merge.head, "a", "Abort merge", function()
-      merge.abort()
-      a.util.scheduler()
-      status.refresh(true, "merge_continue")
-    end)
-    :action(
-      "m",
-      "Merge",
-      a.void(function(popup)
-        local branches = git.branch.get_all_branches()
-        local BranchSelectViewBuffer = require("neogit.buffers.branch_select_view")
-        local branch = BranchSelectViewBuffer.new(branches):open_async()
+  local p = popup.builder():name("NeogitMergePopup")
+
+  if in_merge(status) then
+    p:group_heading("Actions")
+      :action("m", "Continue", function()
+        git.merge.continue()
+        a.util.scheduler()
+        status.refresh(true, "merge_continue")
+      end)
+      :action("a", "Abort", function()
+        git.merge.abort()
+        a.util.scheduler()
+        status.refresh(true, "merge_continue")
+      end)
+  else
+    p:switch("f", "ff-only", "Fast-forward only", false)
+      :switch("n", "no-ff", "No fast-forward", false)
+      :switch("b", "Xignore-space-change", "Ignore changes in amount of whitespace", false)
+      :switch("w", "Xignore-all-space", "Ignore whirespace when comparing lines", false)
+      :option("s", "strategy", "", "Strategy", false)
+      :option("X", "strategy-option", "", "Strategy Option", false)
+      :option("A", "Xdiff-algorithm", "", "Diff algorithm", false)
+      :option("S", "gpg-sign", "", "Sign using gpg", false)
+      :group_heading("Actions")
+      :action("m", "Merge", function(popup)
+        local branch = FuzzyFinderBuffer.new(git.branch.get_all_branches()):open_sync()
         if not branch then
           return
         end
 
-        merge.merge(branch, popup:get_arguments())
+        git.merge.merge(branch, popup:get_arguments())
         a.util.scheduler()
         status.refresh(true, "merge")
       end)
-    )
-    :build()
+      :action("e", "Merge and edit message")
+      :action("n", "Merge but don't commit")
+      :action("A", "Absorb")
+      :new_action_group()
+      :action("p", "Preview merge")
+      :action("s", "Squash merge")
+      :action("i", "Dissolve")
+  end
 
-  p:show()
+  p:build():show()
 
   return p
 end
