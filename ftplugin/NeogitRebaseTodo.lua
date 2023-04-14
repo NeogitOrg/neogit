1a25bb8f019223278ff58fa1f2cf3d59ebcda8f9
Cameron <Alleyria@gmail.com>
2023-04-14 21:29:00 +0200

    Use lua for ftplugin files (commit,merge,rebase), and enable treesitter parsers if available.

---
+++ ftplugin/NeogitRebaseTodo.lua
@@ -0,0 +1,16 @@
+if vim.b.did_ftplugin then
+  return
+end
+
+vim.cmd.source("$VIMRUNTIME/ftplugin/gitrebase.vim")
+vim.cmd([[exec 'norm! gg']])
+
+if vim.fn.prevnonblank(".") ~= vim.fn.line(".") then
+  vim.cmd([[startinsert]])
+end
+
+local parser = vim.treesitter.language.get_lang("gitrebase")
+if parser then
+  vim.treesitter.start(0, parser)
+  vim.cmd([[au BufUnload <buffer> lua vim.treesitter.stop(0)]])
+end
