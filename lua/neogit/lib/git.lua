6f8bb489d23b4515a3a9f9fab9f15d259b216151
Cameron <Alleyria@gmail.com>
2023-04-14 16:44:33 +0200

    Adds 'merge' to git module

--- lua/neogit/lib/git.lua
+++ lua/neogit/lib/git.lua
@@ -1,13 +1,14 @@
 local cli = require("neogit.lib.git.cli")
 
 return {
+  cli = cli,
   status = require("neogit.lib.git.status"),
   stash = require("neogit.lib.git.stash"),
   log = require("neogit.lib.git.log"),
   branch = require("neogit.lib.git.branch"),
-  cli = cli,
   diff = require("neogit.lib.git.diff"),
   rebase = require("neogit.lib.git.rebase"),
+  merge = require("neogit.lib.git.merge"),
   cherry_pick = require("neogit.lib.git.cherry_pick"),
   reset = require("neogit.lib.git.reset"),
   remote = require("neogit.lib.git.remote"),
