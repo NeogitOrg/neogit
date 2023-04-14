1a5395fa4f60fa2fc36c5f7dea0e20e79fe2042a
Brian Pritchett <brian@brianpritchett.com>
2023-04-14 07:28:17 -0500

    stylua

--- lua/neogit/popups/fetch.lua
+++ lua/neogit/popups/fetch.lua
@@ -21,35 +21,33 @@ end
 
 function M.create()
   local p = popup
-      .builder()
-      :name("NeogitFetchPopup")
-      :switch("p", "prune", "Prune deleted branches", false)
-      :switch("t", "tags", "Fetch all tags", false)
-      :action("p", "Fetch from pushremote", function(popup)
+    .builder()
+    :name("NeogitFetchPopup")
+    :switch("p", "prune", "Prune deleted branches", false)
+    :switch("t", "tags", "Fetch all tags", false)
+    :action("p", "Fetch from pushremote", function(popup)
+      fetch_from("pushremote", "origin", status.repo.head.branch, popup:get_arguments())
+    end)
+    :action("u", "Fetch from upstream", function(popup)
+      local upstream = git.branch.get_upstream()
+      if not upstream then
+        return
+      end
 
-        fetch_from("pushremote", "origin", status.repo.head.branch, popup:get_arguments())
-      end)
-      :action("u", "Fetch from upstream", function(popup)
+      fetch_from(upstream.remote, upstream.remote, "", popup:get_arguments())
+    end)
+    :action("a", "Fetch from all remotes", function(popup)
+      local args = popup:get_arguments()
+      table.insert(args, "--all")
 
-        local upstream = git.branch.get_upstream()
-        if not upstream then
-          return
-        end
-
-        fetch_from(upstream.remote, upstream.remote, "", popup:get_arguments())
-      end)
-      :action("a", "Fetch from all remotes", function(popup)
-        local args = popup:get_arguments()
-        table.insert(args, "--all")
-
-        fetch_from("all remotes", "", "", args)
-      end)
-      :action("e", "Fetch from elsewhere", function(popup)
-        local remote = input.get_user_input("remote: ")
-        local branch = git.branch.prompt_for_branch()
-        fetch_from(remote .. ' ' .. branch, remote, branch, popup:get_arguments())
-      end)
-      :build()
+      fetch_from("all remotes", "", "", args)
+    end)
+    :action("e", "Fetch from elsewhere", function(popup)
+      local remote = input.get_user_input("remote: ")
+      local branch = git.branch.prompt_for_branch()
+      fetch_from(remote .. " " .. branch, remote, branch, popup:get_arguments())
+    end)
+    :build()
 
   p:show()
 
