27072b68526d0e15c70d169fc7203ea500a7b5d2
Cameron <Alleyria@gmail.com>
2023-05-02 21:23:25 +0200

    Don't show long hash in UI

--- lua/neogit/lib/git/push.lua
+++ lua/neogit/lib/git/push.lua
@@ -20,7 +20,7 @@ local function update_unmerged(state)
 
   state.unmerged.items = util.map(log.list { "@{upstream}.." }, function(v)
     return {
-      name = string.format("%s %s", v.oid, v.description[1] or "<empty>"),
+      name = string.format("%s %s", v.oid:sub(1, 7), v.description[1] or "<empty>"),
       oid = v.oid,
       commit = v,
     }
