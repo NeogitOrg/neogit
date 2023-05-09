f6bbd8af3e38d6abfa6bcc6e6d267d5744f762d0
Cameron <Alleyria@gmail.com>
2023-05-09 12:47:41 +0200

    Rename internal_args to args

--- lua/neogit/buffers/log_view/ui.lua
+++ lua/neogit/buffers/log_view/ui.lua
@@ -6,15 +6,15 @@ local Graph = require("neogit.buffers.common").CommitGraph
 local M = {}
 
 ---@param commits CommitLogEntry[]
----@param internal_args table
+---@param args table
 ---@return table
-function M.View(commits, internal_args)
-  internal_args.details = true
+function M.View(commits, args)
+  args.details = true
 
   return util.filter_map(commits, function(commit)
     if commit.oid then
-      return Commit(commit, internal_args)
-    elseif internal_args.graph then
+      return Commit(commit, args)
+    elseif args.graph then
       return Graph(commit)
     end
   end)
