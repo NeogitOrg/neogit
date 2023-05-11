db8a5600382a04ceaa540f72395beb359c6e001b
Cameron <Alleyria@gmail.com>
2023-05-11 23:35:43 +0200

    Add telescope to CI

--- tests/init.lua
+++ tests/init.lua
@@ -1,5 +1,6 @@
 local cwd = vim.fn.getcwd()
 local plenary_path = cwd .. "/tmp/plenary"
+local telescope_path = cwd .. "/tmp/telescope"
 
 print("Downloading plenary into: ", plenary_path)
 vim.fn.system {
@@ -10,7 +11,17 @@ vim.fn.system {
   plenary_path,
 }
 
+print("Downloading telescope into: ", telescope_path)
+vim.fn.system {
+  "git",
+  "clone",
+  "--depth=1",
+  "https://github.com/nvim-telescope/telescope.nvim",
+  telescope_path,
+}
+
 vim.opt.rtp:prepend(plenary_path)
+vim.opt.rtp:prepend(telescope_path)
 vim.opt.rtp:prepend(cwd)
 
 vim.cmd("runtime plugin/neogit.lua")
