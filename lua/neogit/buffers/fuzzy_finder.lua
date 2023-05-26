13fc6bb9c0ff5c8b4728f395099219723e0bdd98
Tei Roberts <ten3roberts@gmail.com>
2023-05-26 15:34:41 +0200

    feat: async fuzzy finder buffer

--- lua/neogit/buffers/fuzzy_finder.lua
+++ lua/neogit/buffers/fuzzy_finder.lua
@@ -2,8 +2,6 @@ local Finder = require("neogit.lib.finder")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
-local M = {}
-
 local function buffer_height(count)
   if count < (vim.fn.winheight(0) / 2) then
     return count + 2
@@ -18,14 +16,13 @@ end
 ---@field buffer Buffer
 ---@field open_sync function
 ---@field open function
+local M = {}
 
 ---Creates a new FuzzyFinderBuffer
----@param list table
----@param action function|nil Action is not required if calling :open_sync()
+---@param list any[]
 ---@return FuzzyFinderBuffer
-function M.new(list, action)
+function M.new(list)
   local instance = {
-    action = action,
     list = list,
   }
 
@@ -34,38 +31,13 @@ function M.new(list, action)
   return instance
 end
 
-function M:open(opts)
+function M:open(opts, action)
   opts = opts or {
     allow_multi = false,
     layout_config = { height = buffer_height(#self.list) },
   }
 
-  local select_action = function(prompt_bufnr)
-    local selection = {}
-
-    local picker = action_state.get_current_picker(prompt_bufnr)
-    if #picker:get_multi_selection() > 0 then
-      for _, item in ipairs(picker:get_multi_selection()) do
-        table.insert(selection, item[1])
-      end
-    else
-      table.insert(selection, action_state.get_selected_entry()[1])
-    end
-
-    if not selection[1] or selection[1] == "" then
-      return
-    end
-
-    actions.close(prompt_bufnr)
-
-    if opts.allow_multi then
-      self.action(selection)
-    else
-      self.action(selection[1])
-    end
-  end
-
-  Finder.create(opts):add_entries(self.list):add_select_action(select_action):find()
+  Finder.create(opts):add_entries(self.list):find(action)
 end
 
 -- Opens finder in such a way that selected value can be returned to the main thread
@@ -83,6 +55,28 @@ function M:open_sync(...)
 
   rx()
   return result
+end
+
+---@param opts FinderOpts
+---@return any|nil
+--- Asynchronously prompt the user for the selection, and return the selected item or nil if aborted.
+function M:open_async(opts)
+  opts = opts or {
+    allow_multi = false,
+    layout_config = { height = buffer_height(#self.list) },
+  }
+
+  return Finder.create(opts):add_entries(self.list):find_async()
+end
+
+function M.test()
+  local async = require("plenary.async")
+  async.run(function()
+    local buffer = M.new { "a", "b", "c" }
+    local item = buffer:open_async {}
+
+    print("Selected: " .. vim.inspect(item))
+  end)
 end
 
 return M
