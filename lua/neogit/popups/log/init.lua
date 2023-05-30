b2d8086251d3abcd46783d8d0658560dc4c26315
Cameron <Alleyria@gmail.com>
2023-05-30 20:55:42 +0200

    Refactor: Split up log popup

--- lua/neogit/popups/log/init.lua
+++ lua/neogit/popups/log/init.lua
@@ -1,10 +1,5 @@
 local popup = require("neogit.lib.popup")
-local git = require("neogit.lib.git")
-local util = require("neogit.lib.util")
-
-local LogViewBuffer = require("neogit.buffers.log_view")
-local ReflogViewBuffer = require("neogit.buffers.reflog_view")
-local FuzzyFinderBuffer = require("neogit.buffers.fuzzy_finder")
+local actions = require("neogit.popups.log.actions")
 
 local M = {}
 
@@ -28,18 +23,10 @@ function M.create()
     -- TODO: Activation should be "--", and should open file-select fuzzy finder, defaulting to the filepath under the
     -- cursor if there is one. Needs to get passed into #files() down the line, too.
     -- :option("-", "--", "", "Limit to files")
-    :switch(
-      "f",
-      "follow",
-      "Follow renames when showing single-file log"
-    )
+    :switch("f", "follow", "Follow renames when showing single-file log")
     :arg_heading("Commit Ordering")
     -- :switch("o", "xxx-order", "Order commits by", false) TODO: Build multi-selector switch
-    :switch(
-      "r",
-      "reverse",
-      "Reverse order"
-    )
+    :switch("r", "reverse", "Reverse order")
     :arg_heading("Formatting")
     :switch("g", "graph", "Show graph", { enabled = true, internal = true })
     :switch("c", "color", "Show graph in color")
@@ -47,69 +34,23 @@ function M.create()
     :switch("S", "show-signature", "Show signatures", { key_prefix = "=" })
     -- :switch("h", "header", "Show header", { cli_prefix = "++" }) TODO: Need to figure out how this works
     -- :switch("p", "patch", "Show diffs")
-    :switch(
-      "s",
-      "stat",
-      "Show diffstats"
-    )
+    :switch("s", "stat", "Show diffstats")
     :group_heading("Log")
-    :action("l", "current", function(popup)
-      LogViewBuffer.new(git.log.list(popup:get_arguments()), popup:get_internal_arguments()):open()
-    end)
-    :action("h", "HEAD", function(popup)
-      LogViewBuffer.new(
-        git.log.list(util.merge(popup:get_arguments(), { "HEAD" })),
-        popup:get_internal_arguments()
-      )
-        :open()
-    end)
+    :action("l", "current", actions.log_current)
+    :action("h", "HEAD", actions.log_head)
     :action("u", "related")
     :action("o", "other")
     :new_action_group()
-    :action("L", "local branches", function(popup)
-      LogViewBuffer.new(
-        git.log.list(util.merge(popup:get_arguments(), {
-          git.branch.current() and "" or "HEAD",
-          "--branches",
-        })),
-        popup:get_internal_arguments()
-      ):open()
-    end)
-    :action("b", "all branches", function(popup)
-      LogViewBuffer.new(
-        git.log.list(util.merge(popup:get_arguments(), {
-          git.branch.current() and "" or "HEAD",
-          "--branches",
-          "--remotes",
-        })),
-        popup:get_internal_arguments()
-      ):open()
-    end)
-    :action("a", "all references", function(popup)
-      LogViewBuffer.new(
-        git.log.list(util.merge(popup:get_arguments(), {
-          git.branch.current() and "" or "HEAD",
-          "--all",
-        })),
-        popup:get_internal_arguments()
-      ):open()
-    end)
+    :action("L", "local branches", actions.log_local_branches)
+    :action("b", "all branches", actions.log_all_branches)
+    :action("a", "all references", actions.log_all_references)
     :action("B", "matching branches")
     :action("T", "matching tags")
     :action("m", "merged")
     :new_action_group("Reflog")
-    :action("r", "current", function(popup)
-      ReflogViewBuffer.new(git.reflog.list(git.branch.current(), popup:get_arguments())):open()
-    end)
-    :action("H", "HEAD", function(popup)
-      ReflogViewBuffer.new(git.reflog.list("HEAD", popup:get_arguments())):open()
-    end)
-    :action("O", "other", function(popup)
-      local branch = FuzzyFinderBuffer.new(git.branch.get_local_branches()):open_sync()
-      if branch then
-        ReflogViewBuffer.new(git.reflog.list(branch, popup:get_arguments())):open()
-      end
-    end)
+    :action("r", "current", actions.reflog_current)
+    :action("H", "HEAD", actions.reflog_head)
+    :action("O", "other", actions.reflog_other)
     :new_action_group("Other")
     :action("s", "shortlog")
     :build()
