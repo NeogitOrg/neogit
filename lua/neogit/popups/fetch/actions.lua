6828b2010ad136a77f281502c781ddcd30e012e4
Cameron <Alleyria@gmail.com>
2023-06-02 12:31:29 +0200

    Improve fetch actions

--- lua/neogit/popups/fetch/actions.lua
+++ lua/neogit/popups/fetch/actions.lua
@@ -2,8 +2,11 @@ local M = {}
 
 local a = require("plenary.async")
 local git = require("neogit.lib.git")
+local logger = require("neogit.logger")
 local notif = require("neogit.lib.notification")
-local input = require("neogit.lib.input")
+local status = require("neogit.status")
+
+local FuzzyFinderBuffer = require("neogit.buffers.fuzzy_finder")
 
 local function fetch_from(name, remote, branch, args)
   notif.create("Fetching from " .. name)
@@ -12,38 +15,72 @@ local function fetch_from(name, remote, branch, args)
   if res and res.code == 0 then
     a.util.scheduler()
     notif.create("Fetched from " .. name)
+    logger.debug("Fetched from " .. name)
+    status.refresh(true, "fetch_from")
     vim.cmd("do <nomodeline> User NeogitFetchComplete")
+  else
+    logger.error("Failed to fetch from " .. name)
   end
 end
 
 function M.fetch_from_pushremote(popup)
-  fetch_from(
-    "pushremote",
-    "origin",
-    git.repo.head.branch,
-    popup:get_arguments()
-  )
+  local pushRemote = git.branch.pushRemote()
+  if not pushRemote then
+    local remotes = git.remote.list()
+
+    -- TODO: add other URI's as options
+    pushRemote = FuzzyFinderBuffer.new(remotes):open_sync { prompt_prefix = "set pushRemote > " }
+    if not pushRemote then
+      logger.error("No pushremote set")
+      return
+    end
+
+    git.config.set("branch." .. git.repo.head.branch .. ".pushRemote", pushRemote)
+  end
+
+  fetch_from(pushRemote, pushRemote, "", popup:get_arguments())
 end
 
 function M.fetch_from_upstream(popup)
-  local upstream = git.repo.upstream.ref
+  local upstream = git.repo.upstream.remote
+  local args = popup:get_arguments()
+
   if not upstream then
-    return
+    table.insert(args, "--set-upstream")
+    upstream = FuzzyFinderBuffer.new(git.remote.list()):open_sync {
+      prompt_prefix = "set upstream > ",
+    }
+
+    if not upstream then
+      return
+    end
   end
 
-  fetch_from(git.repo.upstream.ref, git.repo.upstream.remote, "", popup:get_arguments())
+
+  fetch_from(upstream, upstream, "", args)
 end
 
 function M.fetch_from_all_remotes(popup)
-  fetch_from("all remotes", "", "", { unpack(popup:get_arguments()), "--all" })
+  local args = popup:get_arguments()
+  table.insert(args, "--all")
+
+  fetch_from("all remotes", "", "", args)
 end
 
--- TODO: Update to use fuzzy branch picker/remote picker
 function M.fetch_from_elsewhere(popup)
-  local remote = input.get_user_input("remote: ")
-  local branch = git.branch.prompt_for_branch()
-  fetch_from(remote .. " " .. branch, remote, branch, popup:get_arguments())
+  local remote = FuzzyFinderBuffer.new(git.remote.list()):open_sync { prompt_prefix = "remote > " }
+  if not remote then
+    logger.error("No remote set")
+    return
+  end
+
+  fetch_from(remote, remote, "", popup:get_arguments())
 end
+
+-- TODO: Select remote, then select branch in second popup
+-- running `git fetch <remote> <branch>`
+-- function M.another_branch(popup)
+-- end
 
 function M.set_variables()
   require("neogit.popups.branch_config").create()
--- lua/neogit/popups/fetch/init.lua
+++ lua/neogit/popups/fetch/init.lua
@@ -1,7 +1,16 @@
-local popup = require("neogit.lib.popup")
 local actions = require("neogit.popups.fetch.actions")
+local git = require("neogit.lib.git")
+local popup = require("neogit.lib.popup")
 
 local M = {}
+
+local function pushRemote_description()
+  return git.branch.pushRemote() or "pushRemote, setting that"
+end
+
+local function upstream_description()
+  return git.repo.upstream.remote or "@{upstream}, creating it"
+end
 
 function M.create()
   local p = popup
@@ -10,16 +19,17 @@ function M.create()
     :switch("p", "prune", "Prune deleted branches")
     :switch("t", "tags", "Fetch all tags")
     :group_heading("Fetch from")
-    :action("p", "pushRemote", actions.fetch_from_pushremote)
-    :action("u", "upstream", actions.fetch_from_upstream)
-    :action("a", "all remotes", actions.fetch_from_all_remotes)
+    :action("p", pushRemote_description(), actions.fetch_from_pushremote)
+    :action("u", upstream_description(), actions.fetch_from_upstream)
     :action("e", "elsewhere", actions.fetch_from_elsewhere)
+    :action("a", "all remotes", actions.fetch_from_all_remotes)
     :new_action_group("Fetch")
     :action("o", "another branch")
     :action("r", "explicit refspec")
     :action("m", "submodules")
     :new_action_group("Configure")
     :action("C", "Set variables...", actions.set_variables)
+    :env({ highlight = git.branch.pushRemote() })
     :build()
 
   p:show()
