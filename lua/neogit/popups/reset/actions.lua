2818f053d26eccf3c26095bb541769816d48e0c0
Cameron <Alleyria@gmail.com>
2023-06-04 13:16:22 +0200

    Refactor/simplify reset actions and use new git.files lib

--- lua/neogit/popups/reset/actions.lua
+++ lua/neogit/popups/reset/actions.lua
@@ -8,59 +8,35 @@ local FuzzyFinderBuffer = require("neogit.buffers.fuzzy_finder")
 
 local M = {}
 
-function M.mixed()
+local function reset(type)
   local commit = CommitSelectViewBuffer.new(git.log.list { "--max-count=256" }):open_async()
   if not commit then
     return
   end
 
-  git.reset.mixed(commit)
+  git.reset[type](commit)
   a.util.scheduler()
-  status.refresh(true, "reset_mixed")
+  status.refresh(true, "reset_" .. type)
+end
+
+function M.mixed()
+  reset("mixed")
 end
 
 function M.soft()
-  local commit = CommitSelectViewBuffer.new(git.log.list { "--max-count=256" }):open_async()
-  if not commit then
-    return
-  end
-
-  git.reset.soft(commit)
-  a.util.scheduler()
-  status.refresh(true, "reset_soft")
+  reset("soft")
 end
 
 function M.hard()
-  local commit = CommitSelectViewBuffer.new(git.log.list { "--max-count=256" }):open_async()
-  if not commit then
-    return
-  end
-
-  git.reset.hard(commit)
-  a.util.scheduler()
-  status.refresh(true, "reset_hard")
+  reset("hard")
 end
 
 function M.keep()
-  local commit = CommitSelectViewBuffer.new(git.log.list { "--max-count=256" }):open_async()
-  if not commit then
-    return
-  end
-
-  git.reset.keep(commit)
-  a.util.scheduler()
-  status.refresh(true, "reset_keep")
+  reset("keep")
 end
 
 function M.index()
-  local commit = CommitSelectViewBuffer.new(git.log.list { "--max-count=256" }):open_async()
-  if not commit then
-    return
-  end
-
-  git.reset.index(commit)
-  a.util.scheduler()
-  status.refresh(true, "reset_index")
+  reset("index")
 end
 
 -- https://github.com/magit/magit/blob/main/lisp/magit-reset.el#L87
@@ -75,23 +51,20 @@ function M.a_file()
     return
   end
 
-  local files =
-    git.cli["ls-files"].full_name.deleted.modified.exclude_standard.deduplicate.call_sync():trim().stdout
-  local diff = git.cli.diff.name_only.args(commit .. "...").call_sync():trim().stdout
-  local all_files = util.deduplicate(util.merge(files, diff))
-  if not all_files[1] then
+  local files = util.deduplicate(util.merge(git.files.all(), git.files.diff(commit)))
+  if not files[1] then
     return
   end
 
   a.util.scheduler()
-  local files = FuzzyFinderBuffer.new(all_files):open_sync { allow_multi = true }
+  local files = FuzzyFinderBuffer.new(files):open_sync { allow_multi = true }
   if not files[1] then
     return
   end
 
   git.reset.file(commit, files)
   a.util.scheduler()
-  status.refresh(true)
+  status.refresh(true, "reset_file")
 end
 
 return M
