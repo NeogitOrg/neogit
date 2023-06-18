1c87e448bffbeb7246df743028ec1be09ea7138c
Cameron <Alleyria@gmail.com>
2023-06-18 14:55:11 +0200

    add: reset popup

---
+++ lua/neogit/popups/reset/init.lua
@@ -0,0 +1,26 @@
+local popup = require("neogit.lib.popup")
+local actions = require("neogit.popups.reset.actions")
+
+local M = {}
+
+function M.create()
+  local p = popup
+    .builder()
+    :name("NeogitResetPopup")
+    :group_heading("Reset")
+    :action("m", "mixed    (HEAD and index)", actions.mixed)
+    :action("s", "soft     (HEAD only)", actions.soft)
+    :action("h", "hard     (HEAD, index and files)", actions.hard)
+    :action("k", "keep     (HEAD and index, keeping uncommitted)", actions.keep)
+    :action("i", "index    (only)", actions.index)
+    :action("w", "worktree (only)")
+    :group_heading("")
+    :action("f", "a file", actions.a_file)
+    :build()
+
+  p:show()
+
+  return p
+end
+
+return M
