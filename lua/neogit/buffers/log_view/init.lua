39894d7ef56936b1bef8d75e51b1b999e86cc937
Cameron <Alleyria@gmail.com>
2023-06-18 15:09:58 +0200

    update: log view

--- lua/neogit/buffers/log_view/init.lua
+++ lua/neogit/buffers/log_view/init.lua
@@ -1,63 +1,79 @@
 local Buffer = require("neogit.lib.buffer")
-local CommitViewBuffer = require("neogit.buffers.commit_view")
+local util = require("neogit.lib.util")
 local ui = require("neogit.buffers.log_view.ui")
 local config = require("neogit.config")
 
+local CommitViewBuffer = require("neogit.buffers.commit_view")
+local CherryPickPopup = require("neogit.popups.cherry_pick")
+
+---@class LogViewBuffer
+---@field commits CommitLogEntry[]
+---@field internal_args table
 local M = {}
+M.__index = M
 
--- @class LogViewBuffer
--- @field is_open whether the buffer is currently shown
--- @field data the dislayed data
--- @field buffer Buffer
--- @see CommitInfo
--- @see Buffer
-
---- Creates a new LogViewBuffer
--- @param data the data to display
--- @param show_graph whether we should also render the graph on the left side
--- @return LogViewBuffer
-function M.new(data, show_graph)
+---Opens a popup for selecting a commit
+---@param commits CommitLogEntry[]|nil
+---@param internal_args table|nil
+---@return LogViewBuffer
+function M.new(commits, internal_args)
   local instance = {
-    is_open = false,
-    data = data,
-    show_graph = show_graph,
+    commits = commits,
+    internal_args = internal_args,
     buffer = nil,
   }
 
-  setmetatable(instance, { __index = M })
+  setmetatable(instance, M)
 
   return instance
 end
 
 function M:close()
-  self.is_open = false
   self.buffer:close()
   self.buffer = nil
 end
 
 function M:open()
-  if self.is_open then
-    return
-  end
-
-  self.is_open = true
   self.buffer = Buffer.create {
     name = "NeogitLogView",
     filetype = "NeogitLogView",
-    kind = "split",
+    kind = "tab",
+    context_highlight = false,
     mappings = {
+      v = {
+        ["A"] = function()
+          local commits = util.filter_map(
+            self.buffer.ui:get_component_stack_in_linewise_selection(),
+            function(c)
+              if c.options.oid then
+                return c.options.oid
+              end
+            end
+          )
+
+          CherryPickPopup.create { commits = commits }
+        end,
+        ["V"] = function()
+          print("TODO: Revert")
+        end,
+      },
       n = {
         ["q"] = function()
           self:close()
         end,
-        ["F10"] = function()
-          self.ui:print_layout_tree { collapse_hidden_components = true }
+        ["<esc>"] = function()
+          self:close()
         end,
-        ["<enter>"] = function(buffer)
+        ["A"] = function()
           local stack = self.buffer.ui:get_component_stack_under_cursor()
-          local c = stack[#stack]
-          buffer:close()
-          CommitViewBuffer.new(self.data[c.position.row_start].oid):open()
+          CherryPickPopup.create { commits = { stack[#stack].options.oid } }
+        end,
+        ["V"] = function()
+          print("TODO: Revert")
+        end,
+        ["<enter>"] = function()
+          local stack = self.buffer.ui:get_component_stack_under_cursor()
+          CommitViewBuffer.new(stack[#stack].options.oid):open()
         end,
         ["<c-k>"] = function(buffer)
           local stack = self.buffer.ui:get_component_stack_under_cursor()
@@ -82,31 +98,33 @@ function M:open()
 
           buffer.ui:update()
           buffer:move_cursor(target.position.row_start)
-          vim.fn.feedkeys("zz")
+          vim.cmd("normal! zz")
         end,
         ["<tab>"] = function()
           local stack = self.buffer.ui:get_component_stack_under_cursor()
           local c = stack[#stack]
 
-          c.children[2]:toggle_hidden()
-          self.buffer.ui:update()
-          vim.fn.feedkeys("zz")
+          if c.children[2] then
+            c.children[2]:toggle_hidden()
+            self.buffer.ui:update()
+          end
         end,
-        ["d"] = function(buffer)
+        ["d"] = function()
           if not config.ensure_integration("diffview") then
             return
           end
+
           local stack = self.buffer.ui:get_component_stack_under_cursor()
-          local c = stack[#stack]
-          buffer:close()
           local dv = require("neogit.integrations.diffview")
-          local commit_id = self.data[c.position.row_start].oid
-          dv.open("log", commit_id)
+          dv.open("log", stack[#stack].options.oid)
         end,
       },
     },
+    after = function()
+      vim.cmd([[setlocal nowrap]])
+    end,
     render = function()
-      return ui.LogView(self.data, self.show_graph)
+      return ui.View(self.commits, self.internal_args)
     end,
   }
 end
