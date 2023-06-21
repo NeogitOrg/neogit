b2d1cd39f36108422c7482e336deea5ed9ae3f15
Cameron <Alleyria@gmail.com>
2023-06-22 00:06:16 +0200

    remove async code from cherry pick lib

--- lua/neogit/lib/git/cherry_pick.lua
+++ lua/neogit/lib/git/cherry_pick.lua
@@ -1,24 +1,17 @@
 local cli = require("neogit.lib.git.cli")
-local logger = require("neogit.logger")
 local notif = require("neogit.lib.notification")
 local util = require("neogit.lib.util")
 
 local M = {}
 
-local a = require("plenary.async")
-
 function M.pick(commits, args)
-  a.util.scheduler()
-
-  local result = cli["cherry-pick"].arg_list({ unpack(args), unpack(commits) }).call()
+  local result = cli["cherry-pick"].arg_list(util.merge(args, commits)).call()
   if result.code ~= 0 then
     notif.create("Cherry Pick failed. Resolve conflicts before continuing", vim.log.levels.ERROR)
   end
 end
 
 function M.apply(commits, args)
-  a.util.scheduler()
-
   local result = cli["cherry-pick"].no_commit.arg_list(util.merge(args, commits)).call()
   if result.code ~= 0 then
     notif.create("Cherry Pick failed. Resolve conflicts before continuing", vim.log.levels.ERROR)
