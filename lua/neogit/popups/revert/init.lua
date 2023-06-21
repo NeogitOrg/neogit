9429c1bb918b65e167b185bb0ec8e6539d498d7e
Cameron <Alleyria@gmail.com>
2023-06-21 21:16:01 +0200

    comment out untested options

--- lua/neogit/popups/revert/init.lua
+++ lua/neogit/popups/revert/init.lua
@@ -8,11 +8,13 @@ function M.create(commit)
     .builder()
     :name("NeogitRevertPopup")
     :option("m", "mainline", "", "Replay merge relative to parent")
+    -- TODO: enabled = true needs to check if incompatible switch is toggled in internal state, and not apply.
+    --       if you enable 'no edit', and revert, next time you load the popup both will be enabled
     :switch("e", "edit", "Edit commit messages", { enabled = true, incompatible = { "no-edit" } })
     :switch("E", "no-edit", "Don't edit commit messages", { incompatible = { "edit" } })
-    :option("s", "strategy", "", "Strategy")
-    :switch("s", "signoff", "Add Signed-off-by lines")
-    :option("S", "gpg-sign", "", "Sign using gpg")
+    -- :option("s", "strategy", "", "Strategy")
+    -- :switch("s", "signoff", "Add Signed-off-by lines")
+    -- :option("S", "gpg-sign", "", "Sign using gpg")
     :action("_", "Revert commit", actions.commits) -- TODO: Support multiple commits
     :action("v", "Revert changes")
     :env({ commit = commit })
