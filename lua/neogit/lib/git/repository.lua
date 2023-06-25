f4c641a6b44583d27c4288715c513193f6117985
Cameron <Alleyria@gmail.com>
2023-06-25 18:42:49 +0200

    Because the repo is now refreshed in async context, just refresh the entire thing every time.

--- lua/neogit/lib/git/repository.lua
+++ lua/neogit/lib/git/repository.lua
@@ -6,6 +6,7 @@ local function empty_state()
     ---The cwd when this was updated.
     ---Used to generate absolute paths
     cwd = ".",
+    git_root = nil,
     head = {
       branch = nil,
       commit_message = "",
@@ -67,95 +68,22 @@ function M.reset(self)
   self.state = empty_state()
 end
 
-function M.refresh(self, lib)
-  local refreshes = {}
+function M.refresh(self)
+  logger.debug("[REPO]: Refreshing START")
 
-  if lib and type(lib) == "table" then
-    if lib.status then
-      self.lib.update_status(self.state)
-      a.util.scheduler()
-    end
-
-    if lib.branch_information then
-      table.insert(refreshes, function()
-        logger.debug("[REPO]: Refreshing branch information")
-        self.lib.update_branch_information(self.state)
-      end)
-    end
-
-    if lib.rebase then
-      table.insert(refreshes, function()
-        logger.debug("[REPO]: Refreshing rebase information")
-        self.lib.update_rebase_status(self.state)
-      end)
-    end
-
-    if lib.cherry_pick then
-      table.insert(refreshes, function()
-        logger.debug("[REPO]: Refreshing cherry-pick information")
-        self.lib.update_cherry_pick_status(self.state)
-      end)
-    end
-
-    if lib.merge then
-      table.insert(refreshes, function()
-        logger.debug("[REPO]: Refreshing merge information")
-        self.lib.update_merge_status(self.state)
-      end)
-    end
-
-    if lib.stashes then
-      table.insert(refreshes, function()
-        logger.debug("[REPO]: Refreshing stash")
-        self.lib.update_stashes(self.state)
-      end)
-    end
-
-    if lib.unpulled then
-      table.insert(refreshes, function()
-        logger.debug("[REPO]: Refreshing unpulled commits")
-        self.lib.update_unpulled(self.state)
-      end)
-    end
-
-    if lib.unmerged then
-      table.insert(refreshes, function()
-        logger.debug("[REPO]: Refreshing unpushed commits")
-        self.lib.update_unmerged(self.state)
-      end)
-    end
-
-    if lib.recent then
-      table.insert(refreshes, function()
-        logger.debug("[REPO]: Refreshing recent commits")
-        self.lib.update_recent(self.state)
-      end)
-    end
-
-    if lib.diffs then
-      local filter = (type(lib) == "table" and type(lib.diffs) == "table") and lib.diffs or nil
-
-      table.insert(refreshes, function()
-        logger.debug("[REPO]: Refreshing diffs")
-        self.lib.update_diffs(self.state, filter)
-      end)
-    end
-  else
-    logger.debug("[REPO]: Refreshing ALL")
-    self.lib.update_status(self.state)
-    a.util.scheduler()
-
-    for name, fn in pairs(self.lib) do
-      table.insert(refreshes, function()
-        logger.debug("[REPO]: Refreshing " .. name)
-        fn(self.state)
-      end)
-    end
+  self.state.git_root = require("neogit.lib.git.cli").git_root()
+  if self.state.git_root == "" then
+    logger.debug("[REPO]: Refreshing ABORTED")
+    return
   end
 
-  logger.debug(string.format("[REPO]: Running %d refresh(es)", #refreshes))
-  a.util.join(refreshes)
-  a.util.scheduler()
+  self.lib.update_status(self.state)
+
+  for name, fn in pairs(self.lib) do
+    logger.debug(string.format("[REPO]: Refreshing %s", name))
+    fn(self.state)
+  end
+
   logger.debug("[REPO]: Refreshes completed")
 end
 
