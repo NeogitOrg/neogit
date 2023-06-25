b0da10c10dd3aa36ef05cf2dcac0b3145d34b57b
Cameron <Alleyria@gmail.com>
2023-06-25 18:36:21 +0200

    Add filewatcher based on gitsigns implementation. This should set up a single watcher on the .git/ dir, and run the callback fn whenever any files are changed.

---
+++ lua/neogit/watcher.lua
@@ -0,0 +1,70 @@
+local M = {}
+
+local uv = vim.loop
+
+local config = require("neogit.config")
+local logger = require("neogit.logger")
+local util = require("neogit.lib.util")
+local status = require("neogit.status")
+local Path = require("plenary.path")
+
+M.watcher = {}
+
+local function git_dir()
+  return Path.new(require("neogit.lib.git").repo.cwd .. "/.git"):absolute()
+end
+
+function M.setup()
+  local gitdir = git_dir()
+  local watcher = M.watch_git_dir(gitdir)
+
+  M.watcher[gitdir] = watcher
+end
+
+-- Adapted from https://github.com/lewis6991/gitsigns.nvim/blob/main/lua/gitsigns/manager.lua#L575
+--- @param gitdir string
+--- @return uv_fs_event_t?
+function M.watch_git_dir(gitdir)
+  if not config.values.auto_refresh then
+    return
+  end
+
+  if M.watcher[gitdir] then
+    logger.debug(string.format("[WATCHER] for '%s' already setup! Bailing.", gitdir))
+    return
+  end
+
+  local watch_gitdir_handler_db = util.debounce_trailing(100, function()
+    status.dispatch_refresh()
+  end)
+
+  logger.debug("[WATCHER] Watching git dir: " .. gitdir)
+
+  local w = assert(uv.new_fs_event())
+
+  w:start(gitdir, {}, function(err, filename, events)
+    if err then
+      logger.error("[WATCHER] Git dir update error: %s", err)
+      return
+    end
+
+    local info = string.format(
+      "[WATCHER] Git dir update: '%s' %s",
+      filename,
+      vim.inspect(events, { indent = "", newline = " " })
+    )
+
+    if filename:match("%.lock$") then
+      logger.debug("%s (ignoring)", info)
+      return
+    end
+
+    logger.debug(info)
+
+    watch_gitdir_handler_db()
+  end)
+
+  return w
+end
+
+return M
