e80ba09e3147f9a3a1a287841568506d4260aa39
Cameron <Alleyria@gmail.com>
2023-06-26 16:37:21 +0200

    Allow console highlights

--- lua/neogit/logger.lua
+++ lua/neogit/logger.lua
@@ -2,7 +2,7 @@ local log = require("plenary.log")
 
 return log.new {
   plugin = "neogit",
-  highlights = false,
+  highlights = vim.env.NEOGIT_LOG_HIGHLIGHTS or false,
   use_console = vim.env.NEOGIT_LOG_CONSOLE or false,
   use_file = vim.env.NEOGIT_LOG_FILE or false,
   level = vim.env.NEOGIT_LOG_LEVEL or "info",
