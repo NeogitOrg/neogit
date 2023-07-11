e5e0d0da172efde601682fe5f76144e25f1fa78f
Cameron <Alleyria@gmail.com>
2023-07-11 19:34:47 +0200

    fix: this is "git_rebase" not "gitrebase"

--- ftplugin/NeogitRebaseTodo.lua
+++ ftplugin/NeogitRebaseTodo.lua
@@ -4,7 +4,7 @@ end
 
 vim.cmd.source("$VIMRUNTIME/ftplugin/gitrebase.vim")
 
-local parser = vim.treesitter.language.get_lang("gitrebase")
+local parser = vim.treesitter.language.get_lang("git_rebase")
 if parser then
   vim.treesitter.start(0, parser)
   vim.cmd([[au BufUnload <buffer> lua vim.treesitter.stop(0)]])
