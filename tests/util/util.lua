f9373e8633492e7cb86d0fbd24292a798f91848b
Price Hiller <price@orion-technologies.io>
2023-07-15 17:12:12 -0500

    test(feat): add utility to remove a given value from a table

--- tests/util/util.lua
+++ tests/util/util.lua
@@ -20,6 +20,17 @@ function M.lists_equal(l1, l2)
   return true
 end
 
+---Removes the given value from the table
+---@param tbl table
+---@param value any
+function M.remove_item_from_table(tbl, value)
+  for index, t_value in ipairs(tbl) do
+    if vim.deep_equal(t_value, value) then
+      table.remove(tbl, index)
+    end
+  end
+end
+
 ---Returns the path to the raw test files directory
 ---@return string
 function M.get_test_files_dir()
