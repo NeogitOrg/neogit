ffa6434626f236496978760b25c869909431728b
Price Hiller <price@orion-technologies.io>
2023-07-15 19:31:34 -0500

    test(refactor): extract create_temp_dir to utils, set its base path

--- tests/util/util.lua
+++ tests/util/util.lua
@@ -38,7 +38,7 @@ function M.get_test_files_dir()
 end
 
 ---Runs a system command and errors if it fails
----@param cmd string Command to be ran
+---@param cmd string | table Command to be ran
 ---@param ignore_err boolean? Whether the error should be ignored
 ---@param error_msg string? The error message to be emitted on command failure
 ---@return string The output of the system command
@@ -52,6 +52,26 @@ function M.system(cmd, ignore_err, error_msg)
     error(error_msg or ("Command failed: ↓\n" .. cmd .. "\nOutput from command: ↓\n" .. output))
   end
   return output
+end
+
+M.neogit_test_base_dir = "/tmp/neogit-testing/"
+
+---Create a temporary directory for use
+---@param suffix string? The suffix to be appended to the temp directory, ideally avoid spaces in your suffix
+---@return string The path to the temporary directory
+function M.create_temp_dir(suffix)
+  if suffix == nil then
+    suffix = ""
+  else
+    suffix = "-" .. suffix
+  end
+
+  if not vim.loop.fs_stat("/tmp/neogit-testing") then
+    M.system("mkdir " .. M.neogit_test_base_dir)
+  end
+  local tmp_dir =
+    vim.trim(M.system(string.format("mktemp -d --suffix=%s -p %s", suffix, M.neogit_test_base_dir)))
+  return tmp_dir
 end
 
 return M
