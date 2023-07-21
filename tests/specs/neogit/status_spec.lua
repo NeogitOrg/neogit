1dfb1a3e489994e4d11ef1b2d228e18f0e9af928
Price Hiller <price@orion-technologies.io>
2023-07-20 22:43:09 -0500

    test(feat): add tests for recent commits

--- tests/specs/neogit/status_spec.lua
+++ tests/specs/neogit/status_spec.lua
@@ -1,6 +1,7 @@
 local eq = assert.are.same
 local status = require("neogit.status")
 local harness = require("tests.util.git_harness")
+local system = require("tests.util.util").system
 local _ = require("tests.mocks.input")
 local in_prepared_repo = harness.in_prepared_repo
 local get_git_status = harness.get_git_status
@@ -17,10 +18,7 @@ local function find(text)
   for index, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, true)) do
     if line:match(text) then
       vim.api.nvim_win_set_cursor(0, { index, 0 })
-      -- print(">" .. tostring(index) .. " " .. line)
-      break
-      -- else
-      --   print(tostring(index) .. " " .. line)
+      return unpack { line, index }
     end
   end
 end
@@ -306,5 +304,113 @@ describe("status buffer", function()
         )
       end)
     )
+  end)
+
+  describe("recent commits", function()
+    local recent_commit_pattern = "Recent commits %(%d+%)"
+
+    local function refresh_status_buffer()
+      act("<c-r>")
+      require("plenary.async").util.block_on(status.reset)
+    end
+
+    local function create_new_commits(message, number_of_commits)
+      system("git stash && git stash clear && git clean -ffdx")
+      local commit_commands =
+        string.format("printf 'Some Content\\n' >> a-file && git add a-file && git commit -m '%s'", message)
+
+      if number_of_commits == 1 then
+        system(commit_commands)
+      else
+        local loop_cmd = string.format(
+          [[
+          COUNT=1
+          while [ "$COUNT" -ne %s ]; do
+            %s
+            COUNT=$((COUNT + 1))
+          done
+          ]],
+          number_of_commits,
+          commit_commands
+        )
+        system(loop_cmd)
+      end
+      refresh_status_buffer()
+    end
+
+    describe("count", function()
+      it(
+        "has the correct number of recent commits",
+        in_prepared_repo(function()
+          local line = find(recent_commit_pattern)
+          local recent_commit_count = tonumber(string.match(line, "%d+"))
+          local repo_commit_count = tonumber(system("git rev-list master --count"))
+          assert.are.equal(recent_commit_count, repo_commit_count)
+        end)
+      )
+
+      it(
+        "has the correct number when there are more commits than recent_commit_count",
+        in_prepared_repo(function()
+          create_new_commits("A commit to increase commit number", 50)
+          local line = find(recent_commit_pattern)
+          local recent_commit_count = tonumber(string.match(line, "%d+"))
+          local repo_commit_count = tonumber(system("git rev-list master --count"))
+          local config_commit_count = 10
+          require("neogit.config").values.status.recent_commit_count = config_commit_count
+
+          print("Recent Commit Count: " .. recent_commit_count)
+          print("Config Commit Count: " .. config_commit_count)
+          print("Repository Commit Count: " .. repo_commit_count)
+          -- Ensures the actual number of recent commits is less than the repo commits.
+          -- The total number of repo commits SHOULD be more than the recent commit count.
+          assert.True(recent_commit_count < repo_commit_count)
+          -- Ensure the number of recent commits is equal to the number specified in the config
+          assert.are.equal(recent_commit_count, config_commit_count)
+        end)
+      )
+    end)
+    describe("content", function()
+      local function get_latest_recent_commit()
+        -- Get the commit right under the "Recent commits" message
+        local _, cursor_row = find(recent_commit_pattern)
+        act("<tab>")
+        vim.api.nvim_win_set_cursor(0, { cursor_row + 1, 0 })
+        local commit_message_line = vim.api.nvim_buf_get_lines(0, cursor_row, cursor_row + 1, true)[1]
+        -- Remove the leading commit hash
+        local commit_message = string.gsub(commit_message_line, "^[a-z0-9]+ ", "")
+
+        return commit_message
+      end
+
+      it(
+        "has correct recent commit information with the default config",
+        in_prepared_repo(function()
+          local new_commit_message = "a commit"
+          create_new_commits(new_commit_message, 1)
+
+          local commit_message = get_latest_recent_commit()
+          print("Got commit message as: " .. commit_message)
+
+          assert.are.same(new_commit_message, commit_message)
+        end)
+      )
+
+      it(
+        "has correct recent commit information with extra author info",
+        in_prepared_repo(function()
+          require("neogit.config").values.status.recent_commit_include_author_info = true
+          local new_commit_message = "a commit"
+          create_new_commits(new_commit_message, 1)
+
+          local commit_message = get_latest_recent_commit()
+          print("Got commit message as: " .. commit_message)
+
+          new_commit_message =
+            string.format("[%s] <%s> %s", "Neogit Test User", "test@neogit.git", new_commit_message)
+          assert.are.same(new_commit_message, commit_message)
+        end)
+      )
+    end)
   end)
 end)
