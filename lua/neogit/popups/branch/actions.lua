86a9dac9466c8fc53b72c7217960d4e7e9066e64
Morgan Mccauley <morgan@mccauley.co.nz>
2023-08-02 20:21:12 +1200

    fix: Use correct module for `is_dirty()` check

--- lua/neogit/popups/branch/actions.lua
+++ lua/neogit/popups/branch/actions.lua
@@ -23,7 +23,7 @@ local function parse_remote_branch_name(ref)
 end
 
 local function spin_off_branch(checkout)
-  if status.is_dirty() and not checkout then
+  if git.status.is_dirty() and not checkout then
     notif.create("Staying on HEAD due to uncommitted changes", vim.log.levels.INFO)
     checkout = true
   end
