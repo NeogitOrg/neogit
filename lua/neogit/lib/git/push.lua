e74f522c09bd2f57f4f88b85f0d4b6863a36eca9
Cameron <Alleyria@gmail.com>
2023-08-08 22:59:23 +0200

    Fix unpushed section

--- lua/neogit/lib/git/push.lua
+++ lua/neogit/lib/git/push.lua
@@ -14,9 +14,11 @@ function M.push_interactive(remote, branch, args)
 end
 
 local function update_unmerged(state)
+  local upstream_unmerged = {}
+  local pushRemote_unmerged = {}
+
   if state.upstream.ref then
-    state.upstream.unmerged = { items = {} }
-    state.upstream.unmerged.items = util.filter_map(log.list { "@{upstream}.." }, function(v)
+    upstream_unmerged = util.filter_map(log.list { "@{upstream}.." }, function(v)
       if v.oid then
         return {
           name = string.format("%s %s", v.oid:sub(1, 7), v.description[1] or "<empty>"),
@@ -29,8 +31,7 @@ local function update_unmerged(state)
 
   local pushRemote = require("neogit.lib.git").branch.pushRemote_ref()
   if pushRemote then
-    state.pushRemote.unmerged = { items = {} }
-    state.pushRemote.unmerged.items = util.filter_map(log.list { pushRemote .. ".." }, function(v)
+    pushRemote_unmerged = util.filter_map(log.list { pushRemote .. ".." }, function(v)
       if v.oid then
         return {
           name = string.format("%s %s", v.oid:sub(1, 7), v.description[1] or "<empty>"),
@@ -40,6 +41,9 @@ local function update_unmerged(state)
       end
     end)
   end
+
+  state.upstream.unmerged.items = upstream_unmerged
+  state.pushRemote.unmerged.items = pushRemote_unmerged
 end
 
 function M.register(meta)
