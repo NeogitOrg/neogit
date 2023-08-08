56bed5a6fd74032c783d48f44114e64acc06f8d5
Cameron <Alleyria@gmail.com>
2023-08-08 16:15:27 +0200

    Add "split_above" as valid kind value

--- lua/neogit/config.lua
+++ lua/neogit/config.lua
@@ -294,12 +294,15 @@ function M.validate_config()
   local function validate_kind(val, name)
     if
       validate_type(val, name, "string")
-      and not vim.tbl_contains({ "split", "vsplit", "tab", "floating", "replace", "auto" }, val)
+      and not vim.tbl_contains(
+        { "split", "vsplit", "split_above", "tab", "floating", "replace", "auto" },
+        val
+      )
     then
       err(
         name,
         string.format(
-          "Expected `%s` to be one of 'split', 'vsplit', 'tab', 'floating', 'replace' or 'auto', got '%s'",
+          "Expected `%s` to be one of 'split', 'vsplit', 'split_above', 'tab', 'floating', 'replace' or 'auto', got '%s'",
           name,
           val
         )
