cbf03bf8b21035252ff28681db64f1d6cbe7f3b3
Cameron <Alleyria@gmail.com>
2023-08-10 22:24:46 +0200

    No need to condense commits

--- lua/neogit/lib/git/revert.lua
+++ lua/neogit/lib/git/revert.lua
@@ -3,18 +3,8 @@ local util = require("neogit.lib.util")
 
 local M = {}
 
-local function condense_commits(commits)
-  if #commits > 1 then
-    local first = commits[1]
-    local last = commits[#commits]
-    commits = { string.format("%s~1..%s", first, last) }
-  end
-
-  return commits
-end
-
 function M.commits(commits, args)
-  return cli.revert.no_commit.arg_list(util.merge(args, condense_commits(commits))).call().code == 0
+  return cli.revert.no_commit.arg_list(util.merge(args, commits)).call().code == 0
 end
 
 function M.continue()
