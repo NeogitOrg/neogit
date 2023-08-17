b0337cb684f5e5135d3b3c8a325e8538c19412f8
Tei Leelo Roberts <ten3roberts@gmail.com>
2023-08-17 16:00:33 +0200

    fix: linting

--- lua/neogit/lib/git/rev_parse.lua
+++ lua/neogit/lib/git/rev_parse.lua
@@ -7,8 +7,7 @@ local cli = require("neogit.lib.git.cli")
 ---@async
 function M.abbreviate_commit(oid)
   assert(oid, "Missing oid")
-  local abbrev= cli["rev-parse"].short.args(oid).call().stdout[1]
-  assert(abbrev and abbrev ~= "", "Failed to abbreviate commit")
+  local abbrev = cli["rev-parse"].short.args(oid).call().stdout[1]
   return abbrev
 end
 
