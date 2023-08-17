b1c032270869faaaf9144438cc010fff1919850e
Cameron <Alleyria@gmail.com>
2023-08-17 21:54:24 +0200

    lint

--- lua/neogit/popups/log/actions.lua
+++ lua/neogit/popups/log/actions.lua
@@ -8,66 +8,64 @@ local ReflogViewBuffer = require("neogit.buffers.reflog_view")
 local FuzzyFinderBuffer = require("neogit.buffers.fuzzy_finder")
 
 function M.log_current(popup)
-  LogViewBuffer.new(
-    git.log.list(popup:get_arguments(), popup:get_internal_arguments()),
-    popup:get_internal_arguments()
-  )
-    :open()
+  local args = popup:get_internal_arguments()
+  local commits = git.log.list(popup:get_arguments(), args)
+
+  LogViewBuffer.new(commits, args):open()
 end
 
 function M.log_head(popup)
-  LogViewBuffer.new(
-    git.log.list(util.merge(popup:get_arguments(), { "HEAD" }), popup:get_internal_arguments()),
-    popup:get_internal_arguments()
-  )
-    :open()
+  local args = popup:get_internal_arguments()
+  local commits = git.log.list(util.merge(popup:get_arguments(), { "HEAD" }), args)
+
+  LogViewBuffer.new(commits, args):open()
 end
 
 function M.log_local_branches(popup)
-  LogViewBuffer.new(
-    git.log.list(util.merge(popup:get_arguments(), {
-      git.repo.head.branch and "" or "HEAD",
-      "--branches",
-    }), popup:get_internal_arguments()),
-    popup:get_internal_arguments()
-  ):open()
+  local args = popup:get_internal_arguments()
+  local commits = git.log.list(
+    util.merge(popup:get_arguments(), { git.branch.is_detached() and "" or "HEAD", "--branches" }),
+    args
+  )
+
+  LogViewBuffer.new(commits, args):open()
 end
 
 function M.log_other(popup)
+  local args = popup:get_internal_arguments()
   local branch = FuzzyFinderBuffer.new(git.branch.get_local_branches()):open_async()
   if branch then
-    -- stylua: ignore
-    LogViewBuffer.new(
-      git.log.list(util.merge(popup:get_arguments(), { branch }), popup:get_internal_arguments()),
-      popup:get_internal_arguments()
-    ):open()
+    local commits = git.log.list(util.merge(popup:get_arguments(), { branch }), args)
+
+    LogViewBuffer.new(commits, args):open()
   end
 end
 
--- TODO: Verify if branch is nil or empty with detatched head
 function M.log_all_branches(popup)
-  LogViewBuffer.new(
-    git.log.list(util.merge(popup:get_arguments(), {
-      git.repo.head.branch and "" or "HEAD",
-      "--branches",
-      "--remotes",
-    }), popup:get_internal_arguments()),
-    popup:get_internal_arguments()
-  ):open()
+  local args = popup:get_internal_arguments()
+  local commits = git.log.list(
+    util.merge(
+      popup:get_arguments(),
+      { git.branch.is_detached() and "" or "HEAD", "--branches", "--remotes" }
+    ),
+    args
+  )
+
+  LogViewBuffer.new(commits, args):open()
 end
 
 function M.log_all_references(popup)
-  LogViewBuffer.new(
-    git.log.list(util.merge(popup:get_arguments(), {
-      git.repo.head.branch and "" or "HEAD",
-      "--all",
-    }), popup:get_internal_arguments()),
-    popup:get_internal_arguments()
-  ):open()
+  local args = popup:get_internal_arguments()
+  local commits = git.log.list(
+    util.merge(popup:get_arguments(), { git.branch.is_detached() and "" or "HEAD", "--all" }),
+    args
+  )
+
+  LogViewBuffer.new(commits, args):open()
 end
 
 function M.reflog_current(popup)
-  ReflogViewBuffer.new(git.reflog.list(git.repo.head.branch, popup:get_arguments())):open()
+  ReflogViewBuffer.new(git.reflog.list(git.branch.is_detached(), popup:get_arguments())):open()
 end
 
 function M.reflog_head(popup)
