a24e3069cbf328dee046a2ffb7e518d9ba6f0d09
Cameron <Alleyria@gmail.com>
2023-08-27 22:51:51 +0200

    Refactor watcher

--- lua/neogit/watcher.lua
+++ lua/neogit/watcher.lua
@@ -6,49 +6,47 @@ local util = require("neogit.lib.util")
 
 local a = require("plenary.async")
 
--- Adapted from https://github.com/lewis6991/gitsigns.nvim/blob/main/lua/gitsigns/manager.lua#L575
+local watch_gitdir_handler = a.void(function()
+  logger.debug("[WATCHER] Dispatching Refresh")
+  require("neogit.status").dispatch_refresh()
+end)
+
+local watch_gitdir_handler_db =
+  util.debounce_trailing(config.values.filewatcher.interval, watch_gitdir_handler)
+
+local fs_event_handler = function(err, filename, events)
+  if err then
+    logger.error(string.format("[WATCHER] Git dir update error: %s", err))
+    return
+  end
+
+  local info = string.format(
+    "[WATCHER] Git dir update: '%s' %s",
+    filename,
+    vim.inspect(events, { indent = "", newline = " " })
+  )
+
+  -- stylua: ignore
+  if
+    filename == nil or
+    filename:match("%.lock$") or
+    filename:match("COMMIT_EDITMSG") or
+    filename:match("~$")
+  then
+    logger.debug(string.format("%s (ignoring)", info))
+    return
+  end
+
+  logger.debug(info)
+  watch_gitdir_handler_db()
+end
+
+-- Adapted from https://github.com/lewis6991/gitsigns.nvim/blob/main/lua/gitsigns/watcher.lua#L103
 --- @param gitdir string
 --- @return uv_fs_event_t
 local function start(gitdir)
-  local watch_gitdir_handler_db = util.debounce_trailing(
-    config.values.filewatcher.interval,
-    a.void(function()
-      logger.debug("[WATCHER] Dispatching Refresh")
-      require("neogit.status").dispatch_refresh()
-    end)
-  )
-
   local w = assert(uv.new_fs_event())
-
-  w:start(
-    gitdir,
-    {},
-    a.void(function(err, filename, events)
-      if err then
-        logger.error(string.format("[WATCHER] Git dir update error: %s", err))
-        return
-      end
-
-      local info = string.format(
-        "[WATCHER] Git dir update: '%s' %s",
-        filename,
-        vim.inspect(events, { indent = "", newline = " " })
-      )
-
-      -- stylua: ignore
-      if
-        filename:match("%.lock$") or
-        filename:match("COMMIT_EDITMSG") or
-        filename:match("~$")
-      then
-        logger.debug(string.format("%s (ignoring)", info))
-        return
-      end
-
-      logger.debug(info)
-      watch_gitdir_handler_db()
-    end)
-  )
+  w:start(gitdir, {}, fs_event_handler)
 
   return w
 end
