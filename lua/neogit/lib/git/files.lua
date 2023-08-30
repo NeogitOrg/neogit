65ca82b41743daadeedf01cf71f56af4851a0c0a
Cameron <Alleyria@gmail.com>
2023-08-30 22:45:52 +0200

    Add ls-tree command to git cli

--- lua/neogit/lib/git/files.lua
+++ lua/neogit/lib/git/files.lua
@@ -6,6 +6,10 @@ function M.all()
   return cli["ls-files"].full_name.deleted.modified.exclude_standard.deduplicate.call_sync():trim().stdout
 end
 
+function M.all_tree()
+  return cli["ls-tree"].full_tree.name_only.recursive.args("HEAD").call_sync():trim().stdout
+end
+
 function M.diff(commit)
   return cli.diff.name_only.args(commit .. "...").call_sync():trim().stdout
 end
