806267c681f6c86dfc17c956888b404df5e17490
Cameron <Alleyria@gmail.com>
2023-09-04 21:53:38 +0200

    Auto-fetch functions

--- lua/neogit/lib/git/fetch.lua
+++ lua/neogit/lib/git/fetch.lua
@@ -11,4 +11,21 @@ function M.fetch_interactive(remote, branch, args)
   return cli.fetch.args(remote or "", branch or "").arg_list(args).call_interactive()
 end
 
+function M.fetch_upstream()
+  local repo = require("neogit.lib.git").repo
+  if repo.upstream.branch and repo.upstream.remote then
+    cli.fetch.args(repo.upstream.remote, repo.upstream.branch).call_ignoring_exit_code()
+  end
+end
+
+function M.fetch_pushRemote()
+  local branch = require("neogit.lib.git.branch")
+  if not branch.pushRemote_ref() then
+    return
+  end
+
+  local remote, branch = branch.pushRemote_ref():match("^([^/]*)/(.*)$")
+  cli.fetch.args(remote, branch).call_ignoring_exit_code()
+end
+
 return M
