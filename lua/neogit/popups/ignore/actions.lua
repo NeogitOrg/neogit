5a21c1074cffaf94b075e6b465a6b15981919c0d
Cameron <Alleyria@gmail.com>
2023-10-09 22:44:06 +0200

    Add private/global gitignore

--- lua/neogit/popups/ignore/actions.lua
+++ lua/neogit/popups/ignore/actions.lua
@@ -1,6 +1,7 @@
 local M = {}
 local Path = require("plenary.path")
 local git = require("neogit.lib.git")
+local operation = require("neogit.operations")
 
 ---@param path
 ---@param rules string[]
@@ -12,8 +13,7 @@ local function add_rules(path, rules)
   path:write(table.concat(rules, "\n") .. "\n", "a+", 438)
 end
 
-local operation = require("neogit.operations")
-M.shared = operation("ignore_shared", function(popup)
+M.shared_toplevel = operation("ignore_shared", function(popup)
   local git_root = git.repo.git_root
 
   local rules = vim.tbl_map(function(v)
@@ -23,7 +23,16 @@ M.shared = operation("ignore_shared", function(popup)
   add_rules(Path:new(git_root, "/.gitignore"), rules)
 end)
 
-M.private = operation("ignore_private", function(popup)
+M.shared_subdirectory = operation("ignore_subdirectory", function(popup)
+  for _, path in ipairs(popup.state.env.paths) do
+    local path = Path:new(path)
+    local parent = path:parent()
+
+    add_rules(Path:new(parent, "/.gitignore"), { path:make_relative(tostring(parent)) })
+  end
+end)
+
+M.private_local = operation("ignore_private", function(popup)
   local git_path = git.repo.git_path()
 
   local rules = vim.tbl_map(function(v)
@@ -33,13 +42,13 @@ M.private = operation("ignore_private", function(popup)
   add_rules(Path:new(git_path, "/info/exclude"), rules)
 end)
 
-M.at_subdirectory = operation("ignore_subdirectory", function(popup)
-  for _, path in ipairs(popup.state.env.paths) do
-    local path = Path:new(path)
-    local parent = path:parent()
+M.private_global = operation("ignore_private_global", function(popup)
+  local rules = vim.tbl_map(function(v)
+    return Path:new(v):make_relative()
+  end, popup.state.env.paths)
 
-    add_rules(Path:new(parent, "/.gitignore"), { path:make_relative(tostring(parent)) })
-  end
+  local excludesFile = git.config.get_global("core.excludesfile"):read()
+  add_rules(Path:new(excludesFile), rules)
 end)
 
 return M
--- lua/neogit/popups/ignore/init.lua
+++ lua/neogit/popups/ignore/init.lua
@@ -6,13 +6,21 @@ local M = {}
 ---@class IgnoreEnv
 ---@field files string[] Abolute paths
 function M.create(env)
+  local excludesFile = require("neogit.lib.git.config").get_global("core.excludesfile")
+
   local p = popup
     .builder()
     :name("NeogitIgnorePopup")
-    :action("t", "shared at top level", actions.shared)
-    :action("s", "shared at subdirectory", actions.at_subdirectory)
-    :action("p", "private (.git/info/exclude)", actions.private)
-    :action("g", "private for all repositories")
+    :group_heading("Gitignore")
+    :action("t", "shared at toplevel             (.gitignore)", actions.shared_toplevel)
+    :action("s", "shared in subdirectory         (path/to/.gitignore)", actions.shared_subdirectory)
+    :action("p", "privately for this repository  (.git/info/exclude)", actions.private_local)
+    :action_if(
+      excludesFile:is_set(),
+      "g",
+      string.format("privately for all repositories (%s)", excludesFile:read()),
+      actions.private_global
+    )
     :env(env)
     :build()
 
