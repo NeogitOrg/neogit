ae5d51b12a441569614acf94287f16b81900d8ca
Cameron <Alleyria@gmail.com>
2023-11-01 22:15:14 +0100

    Formatting. Show global file relative to home dir.

--- lua/neogit/popups/ignore/init.lua
+++ lua/neogit/popups/ignore/init.lua
@@ -1,4 +1,5 @@
 local actions = require("neogit.popups.ignore.actions")
+local Path = require("plenary.path")
 local popup = require("neogit.lib.popup")
 
 local M = {}
@@ -12,13 +13,16 @@ function M.create(env)
     .builder()
     :name("NeogitIgnorePopup")
     :group_heading("Gitignore")
-    :action("t", "shared at toplevel             (.gitignore)", actions.shared_toplevel)
-    :action("s", "shared in subdirectory         (path/to/.gitignore)", actions.shared_subdirectory)
+    :action("t", "shared at top-level            (.gitignore)", actions.shared_toplevel)
+    :action("s", "shared in sub-directory        (path/to/.gitignore)", actions.shared_subdirectory)
     :action("p", "privately for this repository  (.git/info/exclude)", actions.private_local)
     :action_if(
       excludesFile:is_set(),
       "g",
-      string.format("privately for all repositories (%s)", excludesFile:read()),
+      string.format(
+        "privately for all repositories (%s)",
+        "~/" .. Path:new(excludesFile:read()):make_relative(vim.loop.os_homedir())
+      ),
       actions.private_global
     )
     :env(env)
