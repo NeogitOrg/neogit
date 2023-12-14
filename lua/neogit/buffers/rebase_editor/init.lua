dbf457c52c257c11b8c4726bc6b867c3bc6348cd
Cameron <Alleyria@gmail.com>
2023-12-14 23:54:50 +0100

    Use user mappings for rebase editor; add moveup, movedown, submit, abort

--- lua/neogit/buffers/rebase_editor/init.lua
+++ lua/neogit/buffers/rebase_editor/init.lua
@@ -7,7 +7,7 @@ local CommitViewBuffer = require("neogit.buffers.commit_view")
 local M = {}
 
 local function line_action(action)
-  return function()
+  return function(buffer)
     local line = vim.split(vim.api.nvim_get_current_line(), " ")
     if line[1] == "#" then
       table.remove(line, 1)
@@ -18,8 +18,12 @@ local function line_action(action)
       return
     end
 
-    line[1] = action
-    vim.api.nvim_set_current_line(table.concat(line, " "))
+    if line[2] and line[2]:match("%x%x%x%x%x%x%x%x%x%x") and line[1] ~= "Rebase" then
+      line[1] = action
+      vim.api.nvim_set_current_line(table.concat(line, " "))
+      buffer:write()
+    end
+
     vim.cmd("normal! j")
   end
 end
@@ -37,6 +41,9 @@ function M.new(filename, on_close)
 end
 
 function M:open()
+  local mappings = config.get_reversed_rebase_editor_maps()
+  local aborted = false
+
   self.buffer = Buffer.create {
     name = self.filename,
     load = true,
@@ -47,22 +54,38 @@ function M:open()
     readonly = false,
     autocmds = {
       ["BufUnload"] = function()
-        self.on_close()
-        vim.cmd("silent w!")
-        require("neogit.process").defer_show_preview_buffers()
+        if self.on_close then
+          self.on_close(aborted and 1 or 0)
+        end
+
+        if not aborted then
+          require("neogit.process").defer_show_preview_buffers()
+        end
       end,
     },
     mappings = {
       n = {
-        ["q"] = function(buffer)
+        [mappings["Close"]] = function(buffer)
+          if buffer:get_option("modified") and input.get_confirmation("Save changes?") then
+            buffer:write()
+          end
+
           buffer:close(true)
         end,
-        ["p"] = line_action("pick"),
-        ["r"] = line_action("reword"),
-        ["e"] = line_action("edit"),
-        ["s"] = line_action("squash"),
-        ["f"] = line_action("fixup"),
-        ["x"] = function(buffer)
+        [mappings["Submit"]] = function(buffer)
+          buffer:write()
+          buffer:close(true)
+        end,
+        [mappings["Abort"]] = function(buffer)
+          aborted = true
+          buffer:close(true)
+        end,
+        [mappings["Pick"]] = line_action("pick"),
+        [mappings["Reword"]] = line_action("reword"),
+        [mappings["Edit"]] = line_action("edit"),
+        [mappings["Squash"]] = line_action("squash"),
+        [mappings["Fixup"]] = line_action("fixup"),
+        [mappings["Execute"]] = function(buffer)
           local exec = input.get_user_input("Execute: ")
           if not exec or exec == "" then
             return
@@ -70,7 +93,7 @@ function M:open()
 
           buffer:insert_line("exec " .. exec)
         end,
-        ["d"] = function()
+        [mappings["Drop"]] = function()
           local line = vim.api.nvim_get_current_line()
           if line:match("^# ") then
             return
@@ -79,10 +102,16 @@ function M:open()
           vim.api.nvim_set_current_line("# " .. line)
           vim.cmd("normal! j")
         end,
-        ["b"] = function(buffer)
+        [mappings["Break"]] = function(buffer)
           buffer:insert_line("break")
         end,
-        ["<cr>"] = function()
+        [mappings["MoveUp"]] = function()
+          vim.cmd("move -2")
+        end,
+        [mappings["MoveDown"]] = function()
+          vim.cmd("move +1")
+        end,
+        [mappings["OpenCommit"]] = function()
           local oid = vim.api.nvim_get_current_line():match("(%x%x%x%x%x%x%x)")
           if oid then
             CommitViewBuffer.new(oid):open("tab")
