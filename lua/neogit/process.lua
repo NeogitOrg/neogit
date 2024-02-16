46ca7fcc6959add8331ca556b1ce38da4a3d9dd6
Thore Goll <thore.goll@arculus.de>
2024-02-16 08:32:27 +0100

    Make `Process::pty` & `Process:on_partial_line()` optional fields

--- lua/neogit/process.lua
+++ lua/neogit/process.lua
@@ -26,8 +26,8 @@ end
 ---@field result ProcessResult|nil
 ---@field job number|nil
 ---@field stdin number|nil
----@field pty boolean
----@field on_partial_line fun(process: Process, data: string, raw: string) callback on complete lines
+---@field pty boolean|nil
+---@field on_partial_line fun(process: Process, data: string, raw: string)|nil callback on complete lines
 ---@field on_error (fun(res: ProcessResult): boolean) Intercept the error externally, returning true prevents the error from being logged
 local Process = {}
 Process.__index = Process
