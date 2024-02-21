b68987c6465651907dd7db2973ec1f03d46246ed
Cameron <Alleyria@gmail.com>
2024-02-21 21:38:07 +0100

    Clean up popups mappings. these are now only accessible via the api - all the buffers have their own mappings now

--- lua/neogit/popups/init.lua
+++ lua/neogit/popups/init.lua
@@ -40,128 +40,25 @@ end
 --- Returns an array useful for creating mappings for the available popups
 ---@return table<string, Mapping>
 function M.mappings_table()
-  ---@param commit CommitLogEntry|nil
-  ---@return string|nil
-  local function commit_oid(commit)
-    return commit and commit.oid
-  end
-
-  ---@param commits CommitLogEntry[]
-  ---@return string[]
-  local function map_commits(commits)
-    return vim.tbl_map(function(v)
-      return v.oid
-    end, commits)
-  end
-
-  -- TODO: These can either be removed, or modified
   return {
     { "HelpPopup", "Help", M.open("help") },
-    {
-      "DiffPopup",
-      "Diff",
-      M.open("diff", function(f)
-        local section, item = require("neogit.status").get_current_section_item()
-
-        f { section = section, item = item }
-      end),
-    },
+    { "DiffPopup", "Diff", M.open("diff") },
     { "PullPopup", "Pull", M.open("pull") },
-    {
-      "RebasePopup",
-      "Rebase",
-      M.open("rebase", function(f)
-        f { commit = commit_oid(require("neogit.status").get_selection().commit) }
-      end),
-    },
+    { "RebasePopup", "Rebase", M.open("rebase") },
     { "MergePopup", "Merge", M.open("merge") },
-    {
-      "PushPopup",
-      "Push",
-      M.open("push", function(f)
-        f { commit = commit_oid(require("neogit.status").get_selection().commit) }
-      end),
-    },
-    {
-      "CommitPopup",
-      "Commit",
-      M.open("commit", function(f)
-        f { commit = commit_oid(require("neogit.status").get_selection().commit) }
-      end),
-    },
-    {
-      "IgnorePopup",
-      "Ignore",
-      {
-        "nv",
-        M.open("ignore", function(f)
-          f {
-            paths = util.filter_map(require("neogit.status").get_selection().items, function(v)
-              return v.absolute_path
-            end),
-            git_root = git.repo.git_root,
-          }
-        end),
-      },
-    },
-    {
-      "TagPopup",
-      "Tag",
-      M.open("tag", function(f)
-        f { commit = commit_oid(require("neogit.status").get_selection().commit) }
-      end),
-    },
+    { "PushPopup", "Push", M.open("push") },
+    { "CommitPopup", "Commit", M.open("commit") },
+    { "IgnorePopup", "Ignore", M.open("ignore") },
+    { "TagPopup", "Tag", M.open("tag") },
     { "LogPopup", "Log", M.open("log") },
-    {
-      "CherryPickPopup",
-      "Cherry Pick",
-      {
-        "nv",
-        M.open("cherry_pick", function(f)
-          f { commits = util.reverse(map_commits(require("neogit.status").get_selection().commits)) }
-        end),
-      },
-    },
-    {
-      "BranchPopup",
-      "Branch",
-      {
-        "nv",
-        M.open("branch", function(f)
-          f { commits = map_commits(require("neogit.status").get_selection().commits) }
-        end),
-      },
-    },
+    { "CherryPickPopup", "Cherry Pick", M.open("cherry_pick") },
+    { "BranchPopup", "Branch", M.open("branch") },
     { "FetchPopup", "Fetch", M.open("fetch") },
-    {
-      "ResetPopup",
-      "Reset",
-      {
-        "nv",
-        M.open("reset", function(f)
-          f { commit = commit_oid(require("neogit.status").get_selection().commit) }
-        end),
-      },
-    },
-    {
-      "RevertPopup",
-      "Revert",
-      {
-        "nv",
-        M.open("revert", function(f)
-          f { commits = util.reverse(map_commits(require("neogit.status").get_selection().commits)) }
-        end),
-      },
-    },
+    { "ResetPopup", "Reset", M.open("reset") },
+    { "RevertPopup", "Revert", M.open("revert") },
     { "RemotePopup", "Remote", M.open("remote") },
     { "WorktreePopup", "Worktree", M.open("worktree") },
-    {
-      "StashPopup",
-      "Stash",
-      M.open("stash", function(f)
-        f { name = require("neogit.status").status_buffer:get_current_line()[1]:match("^(stash@{%d+})") }
-      end),
-    },
+    { "StashPopup", "Stash", M.open("stash") },
   }
 end
 
