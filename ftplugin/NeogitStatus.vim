fe4fcb3ca763ba9d861e87e29ab2be935f2140e1
Cameron <Alleyria@gmail.com>
2024-03-18 23:15:39 +0100

    Remove ftplugin for old status, and old status buffer

--- ftplugin/NeogitStatus.vim
+++
@@ -1,7 +0,0 @@
-" Only do this when not done yet for this buffer
-if exists("b:did_ftplugin")
-  finish
-endif
-let b:did_ftplugin = 1
-
-au BufWipeout <buffer> lua require 'neogit.status'.close(true)
--- lua/neogit/status.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
--- Some autocmd seems to be calling this still.. Maybe it's cached? No idea.
--- Without this, closing the neogit status buffer will create an error.
-function M.close() end
-
-return M
