NeogitOrg/neogit

commit 988b28698b0eb630dff81d1e2270803cd2647909

Move remote parsing to the remote library.
This seems to make more sense as a matter of organization, at least if
the routine is ever to be used from any other station in the library.

Fix spelling.

lua/neogit/lib/git/remote.lua
@@ -56,4 +56,48 @@ function M.get_url(name)
   return cli.remote.get_url(name).call({ hidden = true }).stdout
 end
 
+function M.parse(url)
+  local info = {
+    url = url,
+    proto = nil,
+    user = nil,
+    host = nil,
+    port = nil,
+    path = nil,
+    repo = nil,
+    owner = nil,
+    repository = nil,
+  }
+  for _, v in pairs { "git", "https", "http", "ssh" } do
+    if url:sub(1, #v) == v then
+      info.proto = v
+      break
+    end
+  end
+  if info.proto ~= nil then
+    if info.proto == "git" then
+      info.user = "git"
+      info.host = url:match([[@([^:]+)]])
+      info.owner = url:match([[:(%w+)/]])
+    else
+      info.user = url:match([[://(%w+):?%w*@]]) -- Strip passwords.
+      info.port = url:match([[:(%d+)]])
+      if info.user ~= nil and info.port ~= nil then
+        info.host = url:match([[@(.*):]])
+      elseif info.user ~= nil then
+        info.host = url:match([[@(.-)/]])
+      elseif info.port ~= nil then
+        info.host = url:match([[//(.-):]])
+      else
+        info.host = url:match([[//(.-)/]])
+      end
+      info.path = url:sub(#info.proto + 4, #url):match([[/(.*)/]])
+      info.owner = info.path -- Strictly for backwards compatibility.
+    end
+    info.repo = url:match([[/(%w+).git]])
+  end
+  info.repository = info.repo
+  return info
+end
+
 return M

lua/neogit/popups/branch/actions.lua
@@ -274,12 +274,6 @@ M.delete_branch = operation("delete_branch", function()
   end
 end)
 
-local function parse_remote_info(service, url)
-  local delim = url:sub(1, 6) == "ssh://" and "/" or ":"
-  local _, _, owner, repo = string.find(url, string.format("%s[^%s]*%s(.+)/(.+)", service, delim, delim))
-  repo, _ = repo:gsub(".git$", "")
-  return { repository = repo, owner = owner, branch_name = git.branch.current() }
-end
 
 M.open_pull_request = operation("open_pull_request", function()
   local template, service

commit 40dc4c4517d1aafeb29bdfcf1674d77799561818

Fix issue where owner and repo have special characters.
988b2869 introduced a regression where special characters prevented
making complete matches. This changes those match patterns to all
non-slash characters rather than word characters.

lua/neogit/lib/git/remote.lua
@@ -83,7 +83,7 @@ function M.parse(url)
     if protocol == "git" then
       user = "git"
       host = url:match([[@([^:]+)]])
-      owner = url:match([[:(%w+)/]])
+      owner = url:match([[:([^/]+)/]])
     else
       user = url:match([[://(%w+):?%w*@]]) -- Strip passwords.
       port = url:match([[:(%d+)]])
@@ -102,7 +102,7 @@ function M.parse(url)
       owner = path -- Strictly for backwards compatibility.
     end
 
-    repository = url:match([[/(%w+)%.git]])
+    repository = url:match([[/([^/]+)%.git]])
   end
 
   return {