6aab26e7d8b4f94e2c5d9ca6ee1368216629d1ce
Cameron <Alleyria@gmail.com>
2024-03-23 20:55:58 +0100

    Trim is automatic now

--- lua/neogit/lib/git/cherry.lua
+++ lua/neogit/lib/git/cherry.lua
@@ -3,7 +3,7 @@ local cli = require("neogit.lib.git.cli")
 local util = require("neogit.lib.util")
 
 function M.list(upstream, head)
-  local result = cli.cherry.verbose.args(upstream, head).call():trim().stdout
+  local result = cli.cherry.verbose.args(upstream, head).call().stdout
   return util.reverse(util.map(result, function(cherry)
     local status, oid, subject = cherry:match("([%+%-]) (%x+) (.*)")
     return { status = status, oid = oid, subject = subject }
