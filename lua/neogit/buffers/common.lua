3b7fa9caca263d200cf8c5f4c69798dd0b274f0f
Yap Jia Hong <yapjiahong-p@proton.me>
2024-04-19 11:52:02 +0800

    feat(M.stash): Moved to more appropriate location

--- lua/neogit/buffers/common.lua
+++ lua/neogit/buffers/common.lua
@@ -334,22 +334,4 @@ M.Grid = Component.new(function(props)
   return col(rendered)
 end)
 
----Parses output of `git stash list` and splits elements into table
-M.Stash = Component.new(function(stashes)
-  local children = {}
-  for _, stash in ipairs(stashes) do
-    -- Split raw output as the stash_id is useful later.
-    local raw = util.split(stash, ":")
-    local stash_id = raw[1] -- stash@{<num>}
-    local stash_msg = raw[2] .. ":" .. raw[3] -- WIP on <branch>: <commit> <msg>"
-    local entry = row({
-      text(stash_id), text(stash_msg)
-    })
-
-    table.insert(children, entry)
-  end
-
-  return col(children)
-end)
-
 return M
