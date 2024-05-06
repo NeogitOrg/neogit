f10084ae6ad75b6c7169dabde70d95e59a0b4c33
Cameron <Alleyria@gmail.com>
2024-05-06 21:43:59 +0200

    Add move merge actions

--- lua/neogit/popups/merge/actions.lua
+++ lua/neogit/popups/merge/actions.lua
@@ -1,4 +1,5 @@
 local M = {}
+local util = require("neogit.lib.util")
 
 local git = require("neogit.lib.git")
 local input = require("neogit.lib.input")
@@ -20,10 +21,72 @@ function M.abort()
 end
 
 function M.merge(popup)
-  local branch = FuzzyFinderBuffer.new(git.refs.list_branches()):open_async()
-  if branch then
-    git.merge.merge(branch, popup:get_arguments())
+  local refs = util.merge(
+    { popup.state.env.commit },
+    git.refs.list_branches(),
+    git.refs.list_tags()
+  )
+
+  local ref = FuzzyFinderBuffer.new(refs):open_async()
+  if ref then
+    local args = popup:get_arguments()
+    table.insert(args, "--no-edit")
+    git.merge.merge(ref, args)
   end
 end
 
+function M.squash(popup)
+  local refs = util.merge(
+    { popup.state.env.commit },
+    git.refs.list_branches(),
+    git.refs.list_tags()
+  )
+
+  local ref = FuzzyFinderBuffer.new(refs):open_async()
+  if ref then
+    local args = popup:get_arguments()
+    table.insert(args, "--squash")
+    git.merge.merge(ref, args)
+  end
+end
+
+function M.merge_edit(popup)
+  local refs = util.merge(
+    { popup.state.env.commit },
+    git.refs.list_branches(),
+    git.refs.list_tags()
+  )
+
+  local ref = FuzzyFinderBuffer.new(refs):open_async()
+  if ref then
+    local args = popup:get_arguments()
+    table.insert(args, "--edit")
+    util.remove_item_from_table(args, "--ff-only")
+    if not vim.tbl_contains(args, "--no-ff") then
+      table.insert(args, "--no-ff")
+    end
+
+    git.merge.merge(ref, args)
+  end
+end
+
+function M.merge_nocommit(popup)
+  local refs = util.merge(
+    { popup.state.env.commit },
+    git.refs.list_branches(),
+    git.refs.list_tags()
+  )
+
+  local ref = FuzzyFinderBuffer.new(refs):open_async()
+  if ref then
+    local args = popup:get_arguments()
+    table.insert(args, "--no-commit")
+    util.remove_item_from_table(args, "--ff-only")
+    if not vim.tbl_contains(args, "--no-ff") then
+      table.insert(args, "--no-ff")
+    end
+
+    git.merge.merge(ref, args)
+  end
+end
 return M
--- lua/neogit/popups/merge/init.lua
+++ lua/neogit/popups/merge/init.lua
@@ -3,7 +3,7 @@ local actions = require("neogit.popups.merge.actions")
 
 local M = {}
 
-function M.create()
+function M.create(env)
   local in_merge = actions.in_merge()
   local p = popup
     .builder()
@@ -13,6 +13,14 @@ function M.create()
     :action_if(in_merge, "a", "Abort merge", actions.abort)
     :switch_if(not in_merge, "f", "ff-only", "Fast-forward only", { incompatible = { "no-ff" } })
     :switch_if(not in_merge, "n", "no-ff", "No fast-forward", { incompatible = { "ff-only" } })
+    :option_if(not in_merge, "s", "strategy", "", "Strategy", {
+      choices = { "resolve", "recursive", "octopus", "ours", "subtree" },
+      key_prefix = "-",
+    })
+    :option_if(not in_merge, "X", "strategy-option", "", "Strategy Option", {
+      choices = { "ours", "theirs", "patience" },
+      key_prefix = "-",
+    })
     :switch_if(
       not in_merge,
       "b",
@@ -27,14 +35,6 @@ function M.create()
       "Ignore whitespace when comparing lines",
       { cli_prefix = "-" }
     )
-    :option_if(not in_merge, "s", "strategy", "", "Strategy", {
-      choices = { "resolve", "recursive", "octopus", "ours", "subtree" },
-      key_prefix = "-",
-    })
-    :option_if(not in_merge, "X", "strategy-option", "", "Strategy Option", {
-      choices = { "ours", "theirs", "patience" },
-      key_prefix = "-",
-    })
     :option_if(not in_merge, "A", "Xdiff-algorithm", "", "Diff algorithm", {
       choices = { "default", "minimal", "patience", "histogram" },
       cli_prefix = "-",
@@ -43,14 +43,15 @@ function M.create()
     :option_if(not in_merge, "S", "gpg-sign", "", "Sign using gpg", { key_prefix = "-" })
     :group_heading_if(not in_merge, "Actions")
     :action_if(not in_merge, "m", "Merge", actions.merge)
-    :action_if(not in_merge, "e", "Merge and edit message") -- https://github.com/magit/magit/blob/main/lisp/magit-merge.el#L105
-    :action_if(not in_merge, "n", "Merge but don't commit") -- https://github.com/magit/magit/blob/main/lisp/magit-merge.el#L119
-    :action_if(not in_merge, "A", "Absorb") -- https://github.com/magit/magit/blob/main/lisp/magit-merge.el#L158
+    :action_if(not in_merge, "e", "Merge and edit message", actions.merge_edit)
+    :action_if(not in_merge, "n", "Merge but don't commit", actions.merge_nocommit)
+    :action_if(not in_merge, "a", "Absorb") -- https://github.com/magit/magit/blob/main/lisp/magit-merge.el#L158
     :new_action_group_if(not in_merge, "")
     :action_if(not in_merge, "p", "Preview merge") -- https://github.com/magit/magit/blob/main/lisp/magit-merge.el#L225
-    :action_if(not in_merge, "s", "Squash merge") -- -- https://github.com/magit/magit/blob/main/lisp/magit-merge.el#L217
     :group_heading_if(not in_merge, "")
+    :action_if(not in_merge, "s", "Squash merge", actions.squash)
     :action_if(not in_merge, "i", "Dissolve") -- https://github.com/magit/magit/blob/main/lisp/magit-merge.el#L131
+    :env(env)
     :build()
 
   p:show()
