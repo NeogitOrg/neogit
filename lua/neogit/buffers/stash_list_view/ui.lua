b99080a77e842759b746d421f90ba497b161e420
Alphabets, Alphabets <yapjiahong-p@proton.me>
2024-05-06 13:15:55 +0800

    fix(draw): Updated drawing code

--- lua/neogit/buffers/stash_list_view/ui.lua
+++ lua/neogit/buffers/stash_list_view/ui.lua
@@ -1,38 +1,39 @@
 local Ui = require("neogit.lib.ui")
 local Component = require("neogit.lib.ui.component")
+local util = require("neogit.lib.util")
 
 local text = Ui.text
 local col = Ui.col
 local row = Ui.row
 
-local util = require("neogit.lib.util")
-
-local StashEntry = require("neogit.lib.git.stash").StashEntry
+-- local StashEntry = require("neogit.lib.git.stash").StashEntry
 
 local M = {}
 
 ---Parses output of `git stash list` and splits elements into table
-M.Stash = Component.new(function(stashes)
+M.Stash = Component.new(function(stash)
   local children = {}
-  for _, stash in ipairs(stashes) do
-    -- Split raw output as the stash_id is useful later.
-    local raw = util.split(stash, ":")
-    local stash_id = raw[1] -- stash@{<num>}
-    local stash_msg = raw[2] .. ":" .. raw[3] -- WIP on <branch>: <commit> <msg>"
-    local entry = row({
-      text(stash_id), text(stash_msg)
-    })
-
-    table.insert(children, entry)
-  end
-
-  return col(children)
+  local table = table.concat({" stash@{", stash.idx, "}" }, "")
+  return col({
+    row({
+      text.highlight("Comment")(label),
+      text(" "),
+      text(stash.message),
+    }, {
+      virtual_text = {
+        { " ", "Constant" },
+        { stash.rel_date, "Special" },
+      },
+    }),
+  }, { oid = label })
 end)
 
 ---@param stashes StashEntry[]
 ---@return table
 function M.View(stashes)
-  return M.Stash(stashes)
+  return util.map(stashes, function(stash)
+    return M.Stash(stash)
+  end)
 end
 
 return M
