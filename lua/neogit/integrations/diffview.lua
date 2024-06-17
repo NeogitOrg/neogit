96ae003e6ccdb827e0d527600ff28ab6606f52d8
Cameron Ring <cameron@cs.stanford.edu>
2024-06-16 23:20:19 -0700

    Fix #1380

--- lua/neogit/integrations/diffview.lua
+++ lua/neogit/integrations/diffview.lua
@@ -102,7 +102,7 @@ local function get_local_diff_view(section_name, item_name, opts)
 
   view:on_files_staged(a.void(function(_)
     if status.is_open() then
-      status.instance():dispatch_refresh({ update_diffs = true }, "on_files_staged")
+      status.instance():dispatch_refresh({ update_diffs = { "staged:*" } }, "on_files_staged")
     end
 
     view:update_files()
