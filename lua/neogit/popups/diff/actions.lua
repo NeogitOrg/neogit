5f80f6090d7321ea8a60888f0cd69772f0ecdd2a
Cameron <Alleyria@gmail.com>
2024-09-28 22:35:39 +0200

    Simplify diff interface now that telescope input can handle refspecs better

--- lua/neogit/popups/diff/actions.lua
+++ lua/neogit/popups/diff/actions.lua
@@ -19,84 +19,38 @@ function M.this(popup)
 end
 
 function M.range(popup)
-  local current = git.branch.current()
-
-  local common_ranges = {}
-  if current then
-    local branches_to_compare = {}
-
-    local base_branch = git.branch.base_branch()
-    local have_base_branch = base_branch ~= nil and base_branch ~= ""
-    if have_base_branch then
-      table.insert(branches_to_compare, base_branch)
-    end
-
-    local upstream = git.branch.upstream("HEAD")
-    if upstream ~= nil and upstream ~= "" then
-      table.insert(branches_to_compare, upstream)
-    end
-
-    branches_to_compare = util.deduplicate(branches_to_compare)
-    util.remove_item_from_table(branches_to_compare, current)
-
-    for _, branch in pairs(branches_to_compare) do
-      table.insert(common_ranges, branch .. "...HEAD")
-      table.insert(common_ranges, branch .. "..HEAD")
-    end
-
-    if not have_base_branch then
-      table.insert(common_ranges, "(neogit.baseBranch not set)")
-    end
-  end
-
-  local options = util.deduplicate(util.merge({ "(select first)", "(custom range)" }, common_ranges))
-
-  local range = nil
-  local selection = FuzzyFinderBuffer.new(options):open_async { prompt_prefix = "Diff" }
-  if not selection then
-    return
-  end
-
-  if selection == "(select first)" then
-    local options = util.deduplicate(
-      util.merge(
-        { git.branch.current() or "HEAD" },
-        git.branch.get_all_branches(false),
-        git.tag.list(),
-        git.refs.heads()
-      )
+  local options = util.deduplicate(
+    util.merge(
+      { git.branch.current() or "HEAD" },
+      git.branch.get_all_branches(false),
+      git.tag.list(),
+      git.refs.heads()
     )
+  )
 
-    local first_ref = FuzzyFinderBuffer.new(options):open_async { prompt_prefix = "Diff" }
-    if not first_ref then
-      return
-    end
-
-    local second_ref = FuzzyFinderBuffer.new(options)
-      :open_async { prompt_prefix = 'Diff from "' .. first_ref .. '" to ' }
-    if not first_ref then
-      return
-    end
-
-    options = { first_ref .. "..." .. second_ref, first_ref .. ".." .. second_ref }
-    local selected_range = FuzzyFinderBuffer.new(options):open_async {
-      prompt_prefix = 'Diff from merge-base or from "' .. first_ref .. '"?',
-    }
-    if not selected_range then
-      return
-    else
-      range = selected_range
-    end
-  elseif selection == "(custom range)" then
-    range = input.get_user_input("Diff for range", { strip_spaces = true })
-  elseif selection == "(neogit.baseBranch not set)" then
+  local range_from = FuzzyFinderBuffer.new(options):open_async { prompt_prefix = "Diff for range from" }
+  if not range_from then
     return
-  else
-    range = selection
   end
+
+  local range_to = FuzzyFinderBuffer.new(options):open_async { prompt_prefix = "Diff from " .. range_from .. " to" }
+  if not range_to then
+    return
+  end
+
+  local choices = {
+    "&1. " .. range_from .. ".." .. range_to,
+    "&2. " .. range_from .. "..." .. range_to,
+    "&3. Cancel"
+  }
+  local choice = input.get_choice("Select range", { values = choices, default = #choices })
 
   popup:close()
-  diffview.open("range", range)
+  if choice == "1" then
+    diffview.open("range", range_from .. ".." .. range_to)
+  elseif choice == "2" then
+    diffview.open("range", range_from .. "..." .. range_to)
+  end
 end
 
 function M.worktree(popup)
