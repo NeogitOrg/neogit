557d57b5335675c9a57c7e68ee22b376b23a6a73
Cameron <Alleyria@gmail.com>
2024-10-21 22:59:04 +0200

    Add annotations

--- lua/neogit/lib/git/files.lua
+++ lua/neogit/lib/git/files.lua
@@ -3,12 +3,14 @@ local git = require("neogit.lib.git")
 ---@class NeogitGitFiles
 local M = {}
 
+---@return string[]
 function M.all()
   return git.cli["ls-files"].full_name.deleted.modified.exclude_standard.deduplicate.call({
     hidden = true,
   }).stdout
 end
 
+---@return string[]
 function M.untracked()
   return git.cli["ls-files"].others.exclude_standard.call({ hidden = true }).stdout
 end
@@ -17,10 +19,12 @@ function M.all_tree()
   return git.cli["ls-tree"].full_tree.name_only.recursive.args("HEAD").call({ hidden = true }).stdout
 end
 
+---@return string[]
 function M.diff(commit)
   return git.cli.diff.name_only.args(commit .. "...").call({ hidden = true }).stdout
 end
 
+---@return string
 function M.relpath_from_repository(path)
   local result = git.cli["ls-files"].others.cached.modified.deleted.full_name
     .args(path)
@@ -29,10 +33,14 @@ function M.relpath_from_repository(path)
   return result.stdout[1]
 end
 
+---@param path string
+---@return boolean
 function M.is_tracked(path)
   return git.cli["ls-files"].error_unmatch.files(path).call({ hidden = true, ignore_error = true }).code == 0
 end
 
+---@param paths string[]
+---@return boolean
 function M.untrack(paths)
   return git.cli.rm.cached.files(unpack(paths)).call({ hidden = true }).code == 0
 end
