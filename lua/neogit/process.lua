9470bfd4fcf45bb0ecd1da25c21a63d89dcb413a
Cameron <Alleyria@gmail.com>
2024-10-26 19:14:08 +0200

    Add a spinner to show a process is running.

--- lua/neogit/process.lua
+++ lua/neogit/process.lua
@@ -5,6 +5,9 @@ local config = require("neogit.config")
 local logger = require("neogit.logger")
 local util = require("neogit.lib.util")
 
+local ProcessBuffer = require("neogit.buffers.process")
+local Spinner = require("neogit.spinner")
+
 local api = vim.api
 local fn = vim.fn
 
@@ -17,7 +20,6 @@ local function mask_command(cmd)
 end
 
 ---@class ProcessOpts
----@field buffer ProcessBuffer|nil
 ---@field cmd string[]
 ---@field cwd string|nil
 ---@field env table<string, string>|nil
@@ -44,6 +46,7 @@ end
 ---@field on_partial_line fun(process: Process, data: string)|nil callback on complete lines
 ---@field on_error (fun(res: ProcessResult): boolean) Intercept the error externally, returning false prevents the error from being logged
 ---@field defer_show_preview_buffers fun(): nil
+---@field spinner Spinner|nil
 local Process = {}
 Process.__index = Process
 
@@ -75,6 +78,7 @@ function ProcessResult:trim()
   return self
 end
 
+---@return ProcessResult
 function ProcessResult:remove_ansi()
   self.stdout = vim.tbl_map(remove_ansi_escape_codes, self.stdout)
   self.stderr = vim.tbl_map(remove_ansi_escape_codes, self.stderr)
@@ -87,8 +91,6 @@ ProcessResult.__index = ProcessResult
 ---@param process ProcessOpts
 ---@return Process
 function Process.new(process)
-  process.buffer = require("neogit.buffers.process"):new(process)
-
   return setmetatable(process, Process) ---@class Process
 end
 
@@ -103,7 +105,26 @@ function Process.hide_preview_buffers()
 end
 
 function Process:show_console()
-  self.buffer:show()
+  if self.buffer then
+    self.buffer:show()
+  end
+end
+
+function Process:show_spinner()
+  if self.suppress_console or self.spinner then
+    return
+  end
+
+  self.spinner = Spinner.new("Running: " .. mask_command(table.concat(self.cmd, " ")))
+  self.spinner:start()
+end
+
+function Process:hide_spinner()
+  if not self.spinner then
+    return
+  end
+
+  self.spinner:stop()
 end
 
 function Process:start_timer()
@@ -116,6 +137,7 @@ function Process:start_timer()
     self.timer = timer
 
     local timeout = assert(self.git_hook and 800 or config.values.console_timeout, "no timeout")
+
     timer:start(
       timeout,
       0,
@@ -267,12 +289,14 @@ function Process:spawn(cb)
       self:on_partial_line(line)
     end
 
-    self.buffer:append_partial(line)
+    if self.buffer then
+      self.buffer:append_partial(line)
+    end
   end
 
   local stdout_on_line = function(line)
     insert(res.stdout, line)
-    if not self.suppress_console then
+    if self.buffer and not self.suppress_console then
       self.buffer:append(line)
     end
   end
@@ -281,7 +305,7 @@ function Process:spawn(cb)
 
   local stderr_on_line = function(line)
     insert(res.stderr, line)
-    if not self.suppress_console then
+    if self.buffer and not self.suppress_console then
       self.buffer:append(line)
     end
   end
@@ -297,11 +321,12 @@ function Process:spawn(cb)
     processes[self.job] = nil
     self.result = res
     self:stop_timer()
+    self:hide_spinner()
 
     stdout_cleanup()
     stderr_cleanup()
 
-    if not self.suppress_console then
+    if self.buffer and not self.suppress_console then
       self.buffer:append(string.format("Process exited with code: %d", code))
 
       if not self.buffer:is_visible() and code > 0 and self.on_error(res) then
@@ -360,6 +385,8 @@ function Process:spawn(cb)
   self.stdin = job
 
   if not hide_console then
+    self.buffer = ProcessBuffer:new(self)
+    self:show_spinner()
     self:start_timer()
   end
 
---
+++ lua/neogit/spinner.lua
@@ -0,0 +1,65 @@
+---@class Spinner
+---@field text string
+---@field count number
+---@field interval number
+---@field pattern string[]
+---@field timer uv_timer_t
+local Spinner = {}
+Spinner.__index = Spinner
+
+---@return Spinner
+function Spinner.new(text)
+  local instance = {
+    text = text,
+    interval = 100,
+    count = 0,
+    timer = nil,
+    pattern = {
+      "⠋",
+      "⠙",
+      "⠹",
+      "⠸",
+      "⠼",
+      "⠴",
+      "⠦",
+      "⠧",
+      "⠇",
+      "⠏",
+    },
+  }
+
+  return setmetatable(instance, Spinner)
+end
+
+function Spinner:start()
+  if not self.timer then
+    self.timer = vim.uv.new_timer()
+    self.timer:start(
+      0,
+      self.interval,
+      vim.schedule_wrap(function()
+        self.count = self.count + 1
+        local step = self.pattern[(self.count % #self.pattern) + 1]
+        vim.cmd(string.format("redraw | echomsg '%s %s'", step, self.text))
+      end)
+    )
+  end
+end
+
+function Spinner:stop()
+  if self.timer then
+    local timer = self.timer
+    self.timer = nil
+    timer:stop()
+
+    if not timer:is_closing() then
+      timer:close()
+    end
+  end
+
+  vim.schedule(function()
+    vim.cmd("redraw | echomsg ''")
+  end)
+end
+
+return Spinner
