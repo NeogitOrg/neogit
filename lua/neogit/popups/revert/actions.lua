7aa80b47869babb0fa02c0b6e6e494a140aea005
Cameron <Alleyria@gmail.com>
2025-05-13 10:36:05 +0200

    Add an env getter method to popup to make accessing values nicer.
    Simplify revert popup env by passing in hunk, if it is present

--- lua/neogit/popups/revert/actions.lua
+++ lua/neogit/popups/revert/actions.lua
@@ -63,7 +63,7 @@ function M.changes(popup)
 end
 
 function M.hunk(popup)
-  git.revert.hunk(popup.state.env.item.hunk, popup:get_arguments())
+  git.revert.hunk(popup:get_env("hunk"), popup:get_arguments())
 end
 
 function M.continue()
