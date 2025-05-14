f133b16449a7cbd348e9646d13eeb173e23dce0d
Cameron <Alleyria@gmail.com>
2025-05-14 11:00:20 +0200

    Refactor lib.git.reset to no longer touch notifications/events, as that's the realm of the actions (user io)

---
+++ lua/neogit/lib/event.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+-- TODO: All callers of nvim_exec_autocmd should route through here
+
+---@param name string
+---@param data table?
+function M.send(name, data)
+  assert(name, "event must have name")
+
+  vim.api.nvim_exec_autocmds(
+    "User",
+    {
+      pattern = "Neogit" .. name,
+      modeline = false,
+      data = data
+    }
+  )
+end
+
+return M
