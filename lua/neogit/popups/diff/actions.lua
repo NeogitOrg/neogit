beda8099114a1b2d592e60d0c1cc04cda6affb6a
Chris Roscher <chris@aitai.nl>
2025-05-29 11:32:51 +0200

    formatting

--- lua/neogit/popups/diff/actions.lua
+++ lua/neogit/popups/diff/actions.lua
@@ -29,7 +29,8 @@ function M.range(popup)
   )
 
   local range_from = FuzzyFinderBuffer.new(options):open_async {
-    prompt_prefix = "Diff for range from", refocus_status = false
+    prompt_prefix = "Diff for range from",
+    refocus_status = false,
   }
   if not range_from then
     return
@@ -74,7 +75,7 @@ end
 function M.stash(popup)
   popup:close()
 
-  local selected = FuzzyFinderBuffer.new(git.stash.list()):open_async{ refocus_status = false }
+  local selected = FuzzyFinderBuffer.new(git.stash.list()):open_async { refocus_status = false }
   if selected then
     diffview.open("stashes", selected)
   end
@@ -85,7 +86,7 @@ function M.commit(popup)
 
   local options = util.merge(git.refs.list_branches(), git.refs.list_tags(), git.refs.heads())
 
-  local selected = FuzzyFinderBuffer.new(options):open_async{ refocus_status = false }
+  local selected = FuzzyFinderBuffer.new(options):open_async { refocus_status = false }
   if selected then
     diffview.open("commit", selected)
   end
