8d31b4459a628672065d67fee658e72b73dabf65
Cameron <Alleyria@gmail.com>
2025-05-30 21:51:36 +0200

    fix: push and pull popup work again with detached head

--- lua/neogit/popups/push/init.lua
+++ lua/neogit/popups/push/init.lua
@@ -5,7 +5,8 @@ local git = require("neogit.lib.git")
 local M = {}
 
 function M.create(env)
-  local current = git.branch.current()
+  local current = git.branch.current() or ""
+  local is_detached = git.branch.is_detached()
 
   local p = popup
     .builder()
@@ -17,11 +18,12 @@ function M.create(env)
     :switch("u", "set-upstream", "Set the upstream before pushing")
     :switch("T", "tags", "Include all tags")
     :switch("t", "follow-tags", "Include related annotated tags")
-    :group_heading("Push " .. ((current and (current .. " ")) or "") .. "to")
-    :action("p", git.branch.pushRemote_or_pushDefault_label(), actions.to_pushremote)
-    :action("u", git.branch.upstream_label(), actions.to_upstream)
-    :action("e", "elsewhere", actions.to_elsewhere)
-    :new_action_group("Push")
+    :group_heading_if(not is_detached, "Push " .. current .. " to")
+    :action_if(not is_detached, "p", git.branch.pushRemote_or_pushDefault_label(), actions.to_pushremote)
+    :action_if(not is_detached, "u", git.branch.upstream_label(), actions.to_upstream)
+    :action_if(not is_detached, "e", "elsewhere", actions.to_elsewhere)
+    :group_heading_if(is_detached, "Push")
+    :new_action_group_if(not is_detached, "Push")
     :action("o", "another branch", actions.push_other)
     :action("r", "explicit refspec", actions.explicit_refspec)
     :action("m", "matching branches", actions.matching_branches)
