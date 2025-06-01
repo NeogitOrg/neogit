c73c5d83301fd3eb2d6789683016e707b3176908
Cameron <Alleyria@gmail.com>
2025-06-01 19:11:27 +0200

    Add timeout and specs for launching popup without status buffer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -51,7 +51,9 @@ RSpec.configure do |config|
     end
   end
 
-  # config.around do |example|
-  #   Timeout.timeout(10) { example.call }
-  # end
+  config.around do |example|
+    Timeout.timeout(10) do
+      example.run
+    end
+  end
 end
