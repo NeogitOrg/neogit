8f79cc8f1455c8a58cc7669694353b9a566f6d65
Cameron <Alleyria@gmail.com>
2025-08-11 21:35:05 +0200

    Fix: bad merge

--- spec/popups/help_popup_spec.rb
+++ spec/popups/help_popup_spec.rb
@@ -9,13 +9,8 @@ RSpec.describe "Help Popup", :git, :nvim, :popup do # rubocop:disable RSpec/Empt
       " Commands                            Applying changes       Essential commands  ",
       " $ History          M Remote         <c-s> Stage all        <c-r> Refresh       ",
       " A Cherry Pick      m Merge          K Untrack              <cr> Go to file     ",
-<<<<<<< HEAD
-      " b Branch           p Pull           s Stage                <tab> Toggle        ",
-      " B Bisect           P Push           S Stage unstaged                           ",
-=======
       " b Branch           P Push           s Stage                za,<tab> Toggle     ",
       " B Bisect           p Pull           S Stage unstaged                           ",
->>>>>>> upstream
       " c Commit           Q Command        u Unstage                                  ",
       " d Diff             r Rebase         U Unstage all                              ",
       " f Fetch            t Tag            x Discard                                  ",
